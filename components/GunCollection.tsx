4288be3febbb2be938904dcb0b6b7e70c2f26320
Marcel Weber <m00gendai@m00gendai.home>
2025-10-20 05:23:15 +0200

    Added db lookup for guncollection

--- components/GunCollection.tsx
+++ components/GunCollection.tsx
@@ -5,49 +5,72 @@ import { Appbar, FAB, Menu, Switch, Text, Tooltip, Searchbar, Button, Icon } fro
 import { defaultBottomBarHeight, defaultGridGap, defaultSearchBarHeight, defaultViewPadding } from '../configs';
 import { PREFERENCES } from "../configs_DB"
 import { GunType, MenuVisibility, SortingTypes } from '../interfaces';
-import { getIcon, doSortBy } from '../utils';
+import { getIcon, getSortAlternateValue } from '../utils';
 import { useViewStore } from '../stores/useViewStore';
 import { useGunStore } from '../stores/useGunStore';
 import { usePreferenceStore } from '../stores/usePreferenceStore';
-import { useTagStore } from '../stores/useTagStore';
-import { Checkbox } from 'react-native-paper';
 import GunCard from './GunCard';
 import { search, sorting, tooltips } from '../lib/textTemplates';
 import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
-import BottomBar from './BottomBar';
+import * as schema from "../db/schema"
+import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite"
+import {db} from "../db/client"
+import { eq, lt, gte, ne, and, or, like, asc, desc, exists, isNull, sql, inArray } from 'drizzle-orm';
+import FilterMenu from './FilterMenu';
 
 export default function GunCollection({navigation, route}){
 
-  // Todo: Stricter typing ("stringA" | "stringB" instead of just string)
 
   const [menuVisibility, setMenuVisibility] = useState<MenuVisibility>({sortBy: false, filterBy: false});
 
   const [searchBannerVisible, toggleSearchBannerVisible] = useState<boolean>(false)
   const [searchQuery, setSearchQuery] = useState<string>("")
-
-  const { displayAsGrid, toggleDisplayAsGrid, sortBy, setSortBy, language, setSortGunIcon, sortGunIcon, sortGunsAscending, toggleSortGunsAscending, theme } = usePreferenceStore()
+  const { displayAsGrid, toggleDisplayAsGrid, sortBy, setSortBy, language, setSortGunIcon, sortGunIcon, sortGunsAscending, toggleSortGunsAscending, theme, gunFilterOn } = usePreferenceStore()
   const { mainMenuOpen } = useViewStore()
-  const { gunCollection, setGunCollection, currentGun, setCurrentGun } = useGunStore()
-  const { tags } = useTagStore()
-  const [isFilterOn, setIsFilterOn] = useState<boolean>(false);
-  const [gunList, setGunList] = useState<GunType[]>(gunCollection)
-  const [boxes, setBoxes] = useState<string[]>([])
+  const { setCurrentGun } = useGunStore()
 
-  useEffect(()=>{
-    setGunList(isFilterOn ? gunList : gunCollection)
-  ,[]})
+  const { data: gunData } = useLiveQuery(
+    db.select()
+    .from(schema.gunCollection)
+    .where(
+      and(
+        or(
+          like(schema.gunCollection.model, `%${searchQuery}%`),
+          like(schema.gunCollection.manufacturer, `%${searchQuery}%`)
+        ),
+      )
+    )
+    .orderBy(
+      sortGunsAscending ?
+        sortBy === "alphabetical" ?
+          asc((sql`COALESCE(NULLIF(${schema.gunCollection.manufacturer}, ""), ${schema.gunCollection.model})`))
+          : (sql`
+            CASE
+              WHEN NULLIF(${schema.gunCollection[sortBy]}, "") IS NULL THEN NULL
+              ELSE strftime('%s', ${schema.gunCollection[sortBy]})
+            END ASC NULLS LAST`)
+        :
+        sortBy === "alphabetical" ?
+          desc((sql`COALESCE(NULLIF(${schema.gunCollection.manufacturer}, ""), ${schema.gunCollection.model})`))
+          : (sql`
+            CASE
+                WHEN NULLIF(${schema.gunCollection[sortBy]}, "") IS NULL THEN NULL
+                ELSE strftime('%s', ${schema.gunCollection[sortBy]})
+              END DESC NULLS LAST`)
+    ),
+    [searchQuery, sortGunsAscending, sortBy]
+  )
 
-  useEffect(()=>{
-    const sortedGuns = doSortBy(sortBy, sortGunsAscending, gunCollection) as GunType[]
-    setGunCollection(sortedGuns)
-  
-  },[gunCollection])
-  
+  const { data: tagData } = useLiveQuery(
+    db.select()
+    .from(schema.gunTags)
+  )
+
+
+
   async function handleSortBy(type: SortingTypes){
     setSortGunIcon(getIcon(type))
     setSortBy(type)
-    const sortedGuns = doSortBy(type, sortGunsAscending, gunCollection) as GunType[]
-    setGunCollection(sortedGuns)
     const preferences:string = await AsyncStorage.getItem(PREFERENCES)
     const newPreferences:{[key:string] : string} = preferences == null ? {"sortBy": type} : {...JSON.parse(preferences), "sortBy":type} 
     await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
@@ -59,8 +82,6 @@ export default function GunCollection({navigation, route}){
 
   async function handleSortOrder(){
     toggleSortGunsAscending()
-    const sortedGuns = doSortBy(sortBy, !sortGunsAscending, gunCollection) as GunType[]
-    setGunCollection(sortedGuns)
     const preferences:string = await AsyncStorage.getItem(PREFERENCES)
     const newPreferences:{[key:string] : string} = preferences == null ? {"sortOrderGuns": !sortGunsAscending} : {...JSON.parse(preferences), "sortOrderGuns": !sortGunsAscending} 
     await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
@@ -72,52 +93,6 @@ export default function GunCollection({navigation, route}){
     const newPreferences:{[key:string] : string} = preferences == null ? {"displayAsGrid": !displayAsGrid} : {...JSON.parse(preferences), "displayAsGrid": !displayAsGrid} 
     await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
   } 
-
-  function handleFilterSwitch(){
-    setIsFilterOn(!isFilterOn)
-    handleFiltering()
-  }
-
-  function handleFilterPress(tag:{label:string, status:boolean}){
-    const indx = boxes.findIndex(item => item === tag.label)
-    let newBoxes:string[] = []
-
-    if(!boxes.includes(tag.label)){
-      newBoxes = [...boxes, tag.label]
-    }
-    if(boxes.includes(tag.label)){
-      newBoxes = boxes.toSpliced(indx, 1)
-    }
-
-    setBoxes(newBoxes)
-    handleFiltering()
-  }
-
-  function handleFiltering(){
-    if(!isFilterOn){
-      setGunList(gunCollection)
-      return
-    }
-
-    if(boxes.length !== 0){
-      const gunsWithTags = gunCollection.filter(gun => {
-        if(gun.tags !== undefined) {
-          return gun.tags.some(tag => boxes.includes(tag));
-        }
-        
-        return false;
-      })
-     setGunList(gunsWithTags)
-    }
-
-    if(boxes.length === 0){
-      setGunList(gunCollection)
-    }
-  }
-
-  useEffect(()=>{
-    handleFiltering()
-  },[boxes, isFilterOn])
 
   function handleSearch(){
     !searchBannerVisible ? startAnimation() : endAnimation()
@@ -168,7 +143,7 @@ export default function GunCollection({navigation, route}){
         </View>
         <View  style={{display: "flex", flexDirection: "row", justifyContent: "flex-end"}}>
           <Appbar.Action icon="magnify" onPress={()=>handleSearch()}/>
-          <Appbar.Action icon="filter" disabled={tags.length === 0 ? true : false} onPress={() =>{handleMenu("filterBy", true)}} />
+          <Appbar.Action icon="filter" disabled={tagData.length === 0 ? true : false} onPress={() =>{handleMenu("filterBy", true)}} />
           <Menu
             visible={menuVisibility.filterBy}
             onDismiss={()=>handleMenu("filterBy", false)}
@@ -176,17 +151,7 @@ export default function GunCollection({navigation, route}){
             anchorPosition='bottom'
             style={{width: Dimensions.get("window").width/1.5}}
           >
-            <View style={{flex: 1, padding: defaultViewPadding}}>
-              <View style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", alignItems: "center"}}>
-                <Text>Filter:</Text>
-                <Switch value={isFilterOn} onValueChange={()=>handleFilterSwitch()} />
-              </View>
-              <View>
-              {tags.map((tag, index)=>{
-                return <Checkbox.Item mode="android" key={`filter_${tag}_${index}`} label={tag.label} status={boxes.includes(tag.label) ? "checked" : "unchecked"} onPress={()=>handleFilterPress(tag)} />
-              })}
-              </View>
-            </View>
+            <FilterMenu collection='gunCollection'/>
           </Menu>
           <Appbar.Action icon={displayAsGrid ? "view-grid" : "format-list-bulleted-type"} onPress={handleDisplaySwitch} />
           <Menu
@@ -199,10 +164,10 @@ export default function GunCollection({navigation, route}){
             <Menu.Item onPress={() => handleSortBy("paidPrice")} title={`${sorting.paidPrice[language]}`} leadingIcon={getIcon("paidPrice")}/>
             <Menu.Item onPress={() => handleSortBy("marketValue")} title={`${sorting.marketValue[language]}`} leadingIcon={getIcon("marketValue")}/>
             <Menu.Item onPress={() => handleSortBy("acquisitionDate")} title={`${sorting.acquisitionDate[language]}`} leadingIcon={getIcon("acquisitionDate")}/>
-            <Menu.Item onPress={() => handleSortBy("lastAdded")} title={`${sorting.lastAdded[language]}`} leadingIcon={getIcon("lastAdded")}/>
-            <Menu.Item onPress={() => handleSortBy("lastModified")} title={`${sorting.lastModified[language]}`} leadingIcon={getIcon("lastModified")}/>
-            <Menu.Item onPress={() => handleSortBy("lastShot")} title={`${sorting.lastShot[language]}`} leadingIcon={getIcon("lastShot")}/>
-            <Menu.Item onPress={() => handleSortBy("lastCleaned")} title={`${sorting.lastCleaned[language]}`} leadingIcon={getIcon("lastCleaned")}/>
+            <Menu.Item onPress={() => handleSortBy("createdAt")} title={`${sorting.lastAdded[language]}`} leadingIcon={getIcon("createdAt")}/>
+            <Menu.Item onPress={() => handleSortBy("lastModifiedAt")} title={`${sorting.lastModified[language]}`} leadingIcon={getIcon("lastModifiedAt")}/>
+            <Menu.Item onPress={() => handleSortBy("lastShotAt")} title={`${sorting.lastShot[language]}`} leadingIcon={getIcon("lastShotAt")}/>
+            <Menu.Item onPress={() => handleSortBy("lastCleanedAt")} title={`${sorting.lastCleaned[language]}`} leadingIcon={getIcon("lastCleanedAt")}/>
           </Menu>
           <Appbar.Action icon={sortGunsAscending ? "arrow-up" : "arrow-down"} onPress={() => handleSortOrder()} />
         </View>
@@ -217,7 +182,9 @@ export default function GunCollection({navigation, route}){
           columnWrapperStyle={{gap: defaultGridGap}} 
           key={`gunCollectionGrid4`} 
           style={{height: "100%", width: "100%", paddingTop: defaultViewPadding, paddingLeft: defaultViewPadding, paddingRight: defaultViewPadding, paddingBottom: 50}} 
-          data={searchQuery !== "" ? gunList.filter(item => item.manufacturer && item.manufacturer.toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) || item.model && item.model.toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) || (Array.isArray(item.caliber) ? item.caliber.join(", ").toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) : "")) : gunList}
+           /*@ts-expect-error*/
+           data={gunData.filter(gun => gunFilterOn ? tagData.filter(tag => tag.active).every(tag => gun.tags?.includes(tag.label)) : gun)} 
+          /*@ts-expect-error*/
           renderItem={({item, index}) => <GunCard gun={item} />}                     
           keyExtractor={gun=>gun.id} 
           ListFooterComponent={<View style={{width: "100%", height: 100}}></View>}
@@ -231,7 +198,9 @@ export default function GunCollection({navigation, route}){
           columnWrapperStyle={{gap: defaultGridGap}} 
           key={`gunCollectionGrid2`} 
           style={{height: "100%", width: "100%", paddingTop: defaultViewPadding, paddingLeft: defaultViewPadding, paddingRight: defaultViewPadding, paddingBottom: 50}} 
-          data={searchQuery !== "" ? gunList.filter(item => item.manufacturer && item.manufacturer.toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) || item.model && item.model.toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) || (Array.isArray(item.caliber) ? item.caliber.join(", ").toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) : "")) : gunList}
+           /*@ts-expect-error*/
+           data={gunData.filter(gun => gunFilterOn ? tagData.filter(tag => tag.active).every(tag => gun.tags?.includes(tag.label)) : gun)} 
+          /*@ts-expect-error*/
           renderItem={({item, index}) => <GunCard gun={item} />}                     
           keyExtractor={gun=>gun.id} 
           ListFooterComponent={<View style={{width: "100%", height: 100}}></View>}
@@ -244,15 +213,17 @@ export default function GunCollection({navigation, route}){
           contentContainerStyle={{gap: defaultGridGap}}
           key={`gunCollectionList`} 
           style={{height: "100%", width: "100%", paddingTop: defaultViewPadding, paddingLeft: defaultViewPadding, paddingRight: defaultViewPadding, paddingBottom: 50}} 
-          data={searchQuery !== "" ? gunList.filter(item => item.manufacturer && item.manufacturer.toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) || item.model && item.model.toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) || (Array.isArray(item.caliber) ? item.caliber.join(", ").toLowerCase().replaceAll(".", "").replaceAll(" ", "").includes(searchQuery.toLowerCase().replaceAll(".", "").replaceAll(" ", "")) : "")) : gunList} 
+          /*@ts-expect-error*/
+          data={gunData.filter(gun => gunFilterOn ? tagData.filter(tag => tag.active).every(tag => gun.tags?.includes(tag.label)) : gun)} 
+          /*@ts-expect-error*/
           renderItem={({item, index}) => <GunCard gun={item} />}      
           keyExtractor={gun=>gun.id} 
           ListFooterComponent={<View style={{width: "100%", height: 100}}></View>}
           ListEmptyComponent={null}
         />
       }
-      <BottomBar screen={route.name}/>
-      <Animated.View style={[{position: "absolute", bottom: defaultBottomBarHeight+defaultViewPadding, right: 0, margin: 16, width: 56, height: 56, backgroundColor: "transparent", display: "flex", justifyContent: "center", alignItems: "center"}, gunCollection.length === 0 ? pulsate : null]}>
+     
+      <Animated.View style={[{position: "absolute", bottom: defaultBottomBarHeight+defaultViewPadding, right: 0, margin: 16, width: 56, height: 56, backgroundColor: "transparent", display: "flex", justifyContent: "center", alignItems: "center"}, gunData.length === 0 ? pulsate : null]}>
         <FAB
           icon="plus"
           onPress={()=>handleFAB()}
