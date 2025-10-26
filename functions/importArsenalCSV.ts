2f02e43fb9de63cad253384ba0a5873aef2419bd
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-10-26 11:57:47 +0000

    adapted for DB and outsourced

---
+++ functions/importArsenalCSV.ts
@@ -0,0 +1,98 @@
+import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
+import Papa from 'papaparse';
+import { unflatten } from 'flat'
+import { AmmoType, GunType, GunTypeStatus } from '../interfaces';
+import * as schema from "../db/schema"
+import { db } from "../db/client"
+import { emptyGunObject } from '../lib/gunDataTemplate';
+import { emptyAmmoObject } from '../lib/ammoDataTemplate';
+
+export default async function importArsenalCSV(data:"gun"|"ammo"){
+
+        const result = await DocumentPicker.getDocumentAsync({copyToCacheDirectory: true})
+
+        if(result.assets === null){
+            return
+        }
+
+        const importableItem:GunType | AmmoType = data === "gun" ? emptyGunObject : emptyAmmoObject
+
+        const content:string = await FileSystem.readAsStringAsync(result.assets[0].uri)
+        
+        const parsed = Papa.parse(content, {header: true, dynamicTyping: true})
+        
+        // The errors are due to GunType expecting string[], but the parsed content is only a string. Maybe a type ImportableGunType[] should be created.
+        // Future me: What the hell dude, the caliber should, nay MUST be a string[], ffs, what were you THINKING past me
+        const unflat:(GunType&GunTypeStatus | AmmoType)[] = parsed.data.map(item => {
+            const unitem:GunType&GunTypeStatus | AmmoType = unflatten(item)
+            /* @ts-expect-error */
+            const filterEmptyTags:string[] = unitem.tags === undefined ? [] : unitem.tags === null ? [] : unitem.tags === "" ? [] : typeof unitem.tags === "string" ? unitem.tags.split(",") : unitem.tags
+
+            if(data === "gun"){
+                const item = unitem as GunType&GunTypeStatus
+                const multiCal:string[] = unitem.caliber === undefined ? [] : unitem.caliber === null ? [] : typeof unitem.caliber === "string" ? unitem.caliber.split(",") : unitem.caliber
+                
+                const readyItem:GunType&GunTypeStatus = {...importableItem as GunType, 
+                    id: item.id,
+                    manufacturer: item.manufacturer,
+                    model: item.model,
+                    manufacturingDate: item.manufacturingDate,
+                    originCountry: item.originCountry,
+                    caliber: multiCal,
+                    serial: item.serial,
+                    permit: item.permit,
+                    acquisitionDate: item.acquisitionDate,
+                    boughtFrom: item.boughtFrom,
+                    mainColor: item.mainColor,
+                    remarks : item.remarks,
+                    images: [],
+                    createdAt: item.createdAt === null ? new Date().getTime() : item.createdAt,
+                    lastModifiedAt: item.lastModifiedAt,
+                    shotCount: item.shotCount,
+                    tags: filterEmptyTags,
+                    lastShotAt: item.lastShotAt,
+                    lastCleanedAt: item.lastCleanedAt,
+                    paidPrice: item.paidPrice,
+                    marketValue: item.marketValue,
+                    cleanInterval: item.cleanInterval,
+                    exFullAuto: item.exFullAuto,
+                    highCapacityMagazine: item.highCapacityMagazine,
+                    short: item.short,
+                    fullAuto: item.fullAuto,
+                    launcher: item.launcher,
+                    decepticon: item.decepticon,
+                    blooptoob: item.blooptoob,
+                    grandfather: item.grandfather,
+                }
+                if(readyItem.model === "P226"){
+                    console.log(readyItem)
+                }
+                return readyItem
+            }
+            if(data === "ammo"){
+                const readyItem = {...importableItem, tags: filterEmptyTags} as AmmoType
+                return readyItem
+            }
+            return null
+        })
+        if(unflat == null){
+            return
+        }
+
+        if(data === "gun"){
+            await db.delete(schema.gunCollection);
+            for(const item of unflat){
+                await db.insert(schema.gunCollection).values(item as GunType&GunTypeStatus)
+            }
+        }
+        if(data === "ammo"){
+            await db.delete(schema.ammoCollection);
+            for(const item of unflat){
+                await db.insert(schema.ammoCollection).values(item as AmmoType)
+            }
+        }
+
+        return unflat.length
+
+}
