337f5915e656c59ee0cfaa7da82a822d68ed24a5
Marcel Weber <m00gendai@m00gendai.home>
2025-10-27 23:40:02 +0100

    No idea about the diff

--- components/MainMenu.tsx
+++ components/MainMenu.tsx
@@ -383,7 +383,7 @@ export default function MainMenu({navigation}){
                     alarm("printAmmoCollection Error", e)
                 }
                 break
-                
+
         }
     }
     
