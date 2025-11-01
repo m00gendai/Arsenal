1619400a3ffb6b0b1156e7f45cc2d1922cac59f2
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-11-01 18:54:08 +0000

    Refactored for new NewText variants

--- components/NewAmmo.tsx
+++ components/NewAmmo.tsx
@@ -23,6 +23,12 @@ import { useAmmoStore } from '../stores/useAmmoStore';
 import * as FileSystem from 'expo-file-system';
 import * as schema from "../db/schema"
 import { db } from "../db/client"
+import { caliberPickerTriggerFields, colorPickerTriggerFields, datePickerTriggerFields, intervalPickerTriggerFields } from '../configs';
+import NewText_DatePicker from './NewText_DatePicker';
+import NewText_ColorPicker from './NewText_ColorPicker';
+import NewText_CaliberPicker from './NewText_CaliberPicker';
+import NewText_IntervalPicker from './NewText_IntervalPicker';
+import NewText_Text from './NewText_Text';
 
 
 export default function NewAmmo({navigation}){
@@ -335,7 +341,15 @@ export default function NewAmmo({navigation}){
                                         
                                 }}>
                                     
-                                    <NewText data={data.name} ammoData={ammoData} setAmmoData={setAmmoData} label={data[language]}/>
+                                    {datePickerTriggerFields.includes(data.name) ? 
+                                        <NewText_DatePicker data={data.name} itemData={ammoData} setItemData={setAmmoData} label={data[language]} /> :
+                                    colorPickerTriggerFields.includes(data.name) ? 
+                                        <NewText_ColorPicker data={data.name} itemData={ammoData} setItemData={setAmmoData} label={data[language]} /> :
+                                    caliberPickerTriggerFields.includes(data.name) ?
+                                        <NewText_CaliberPicker data={data.name} itemData={ammoData} setItemData={setAmmoData} label={data[language]} /> :
+                                    intervalPickerTriggerFields.includes(data.name) ? 
+                                        <NewText_IntervalPicker data={data.name} itemData={ammoData} setItemData={setAmmoData} label={data[language]} /> :
+                                        <NewText_Text data={data.name} itemData={ammoData} setItemData={setAmmoData} label={data[language]} />}
                                 </View>
                             )
                         })}
