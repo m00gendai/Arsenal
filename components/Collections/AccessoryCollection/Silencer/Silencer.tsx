3320bc8ddf79971080dec303986f650f30015596
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-11-06 13:02:36 +0000

    New Collection component templates for Accessory Silencer

---
+++ components/Collections/AccessoryCollection/Silencer/Silencer.tsx
@@ -0,0 +1,229 @@
+import { StyleSheet, View, ScrollView, TouchableNativeFeedback, Pressable, Platform } from 'react-native';
+import { Button, Appbar, Icon, Chip, Text, Dialog, Portal, Modal} from 'react-native-paper';
+import { ammoDataTemplate, ammoRemarks } from "lib/DataTemplates/ammoDataTemplate"
+import { useState} from "react"
+import ImageViewer from "components/ImageViewer"
+import { usePreferenceStore } from 'stores/usePreferenceStore';
+import { useViewStore } from 'stores/useViewStore';
+import { useAmmoStore } from 'stores/useAmmoStore';
+import { ammoDeleteAlert, iosWarningText } from 'lib/textTemplates';
+import { printSingleAmmo } from 'functions/printToPDF';
+import { AmmoType } from 'interfaces';
+import { defaultViewPadding } from 'configs';
+import { alarm } from 'utils';
+import * as Sharing from 'expo-sharing';
+import * as FileSystem from 'expo-file-system';
+import * as schema from "db/schema"
+import { db } from "db/client"
+import { eq } from 'drizzle-orm';
+
+export default function Ammo({navigation}){
+
+    const [lightBoxIndex, setLightBoxIndex] = useState<number>(0)
+    const [dialogVisible, toggleDialogVisible] = useState<boolean>(false)
+
+    const { setSeeAmmoOpen, editAmmoOpen, setEditAmmoOpen, lightBoxOpen, setLightBoxOpen } = useViewStore()
+    const { language, theme, generalSettings, caliberDisplayNameList } = usePreferenceStore()
+    const { currentAmmo, setCurrentAmmo, ammoCollection, setAmmoCollection } = useAmmoStore()
+
+    const [iosWarning, toggleiosWarning] = useState<boolean>(false)
+
+    const showModal = (index:number) => {
+        setLightBoxOpen()
+        setLightBoxIndex(index)
+    }
+
+    const styles = StyleSheet.create({
+        container: {
+            display: "flex",
+            flex: 1,
+            flexWrap: "wrap",
+            flexDirection: "column",
+            height: "100%",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            alignContent: "flex-start",
+            gap: 5,
+            padding: 5,
+        },
+        imageContainer: {
+            width: "100%",
+            aspectRatio: "21/10",
+            flexDirection: "row",
+            flex: 1,
+            alignContent: "center",
+            alignItems: "center",
+            justifyContent: "center"
+        },
+        data: {
+            flex: 1,
+            height: "100%",
+            width: "100%",
+            marginTop: 10
+        },
+    })
+
+    async function deleteItem(ammo:AmmoType){
+            await db.delete(schema.ammoCollection).where(eq(schema.ammoCollection.id, ammo.id));
+            toggleDialogVisible(false)
+            navigation.navigate("AmmoCollection")
+        }
+
+    function handleIosPrint(){
+        toggleiosWarning(true)
+    }
+
+    function handlePrintPress(){
+        toggleiosWarning(false)
+        try{
+            printSingleAmmo(currentAmmo, language, generalSettings.caliberDisplayName, caliberDisplayNameList)
+        }catch(e){
+            alarm("Print Single Ammo Error", e)
+        }
+    }
+
+    async function handleShareImage(img:string){
+        console.log(img)
+        await Sharing.shareAsync(img.includes(FileSystem.documentDirectory) ? img: `${FileSystem.documentDirectory}${img}`)
+    }
+
+    function getShortCaliberName(caliber:string){
+        const match = caliberDisplayNameList.find(obj => obj.name === caliber)
+        // If a match is found, return the displayName, else return the original item
+        return match ? match.displayName : caliber;
+    }
+
+    return(
+        <View style={{flex: 1}}>
+            
+            <Appbar style={{width: "100%"}}>
+                <Appbar.BackAction  onPress={() => navigation.navigate("AmmoCollection")} />
+                <Appbar.Content title={`${currentAmmo.designation} ${currentAmmo.manufacturer ? currentAmmo.manufacturer : ""}`} />
+                <Appbar.Action icon="printer" onPress={()=>Platform.OS === "ios" ? handleIosPrint() : handlePrintPress()} />
+                <Appbar.Action icon="pencil" onPress={()=>navigation.navigate("EditAmmo")} />
+            </Appbar>
+        
+            <View style={styles.container}>   
+                <ScrollView style={{width: "100%"}}>
+                    <View style={{backgroundColor: "transparent"}}>
+                        <ScrollView horizontal style={{width:"100%", aspectRatio: "21/10", marginTop: 10, marginBottom: 10}}>
+                            {Array.from(Array(5).keys()).map((_, index) =>{
+                            
+                                if(currentAmmo.images && index <= currentAmmo.images.length-1){
+                                    return(
+                                        <TouchableNativeFeedback key={`slides_${index}`} onPress={()=>showModal(index)}>
+                                            <View style={styles.imageContainer} >
+                                            <ImageViewer placeholder='ammo' isLightBox={false} selectedImage={currentAmmo.images[index]} /> 
+                                            </View>
+                                        </TouchableNativeFeedback>
+                                    )
+                                }      
+                                if(!currentAmmo.images || currentAmmo.images.length === 0){
+                                    return(
+                                        <TouchableNativeFeedback key={`slides_${index}`}>
+                                            <View style={styles.imageContainer} >
+                                            <ImageViewer placeholder="ammo" isLightBox={false} selectedImage={null} /> 
+                                            </View>
+                                        </TouchableNativeFeedback>
+                                    )
+                                }                   
+                            })}
+                        </ScrollView>
+                    </View>
+                    <View style={styles.data}>
+                        <View style={{flex: 1, flexDirection: "row", flexWrap: "wrap", marginBottom: 10}}>
+                            {currentAmmo.tags?.map((tag, index) =>{
+                                return <View key={`${tag}_${index}`} style={{padding: 5}}><Chip >{tag}</Chip></View>
+                            })}
+                        </View>
+                        {ammoDataTemplate.map((item, index)=>{
+                            if(!generalSettings.emptyFields){
+                                return(
+                                    <View key={`${item.name}`} style={{flex: 1, flexDirection: "column"}} >
+                                        <Text style={{width: "100%", fontSize: 12,}}>{`${item[language]}:`}</Text>
+                                        <Text style={{width: "100%", fontSize: 18, marginBottom: 5, paddingBottom: 5, borderBottomColor: theme.colors.primary, borderBottomWidth: 0.2}}>
+                                        {currentAmmo[item.name] 
+                                                ? item.name === "caliber" 
+                                                    ? generalSettings.caliberDisplayName 
+                                                        ? getShortCaliberName(currentAmmo.caliber)
+                                                        : currentAmmo.caliber
+                                                    : currentAmmo[item.name]
+                                                : ""}
+                                        </Text>
+                                    </View>
+                                )
+                            } else if(currentAmmo[item.name] !== null && currentAmmo[item.name] !== undefined && currentAmmo[item.name] !== "" && currentAmmo[item.name].length !== 0){
+                            return(
+                                <View key={`${item.name}`} style={{flex: 1, flexDirection: "column"}} >
+                                    <Text style={{width: "100%", fontSize: 12,}}>{`${item[language]}:`}</Text>
+                                    <Text style={{width: "100%", fontSize: 18, marginBottom: 5, paddingBottom: 5, borderBottomColor: theme.colors.primary, borderBottomWidth: 0.2}}>
+                                    {currentAmmo[item.name] 
+                                                ? item.name === "caliber" 
+                                                    ? generalSettings.caliberDisplayName 
+                                                        ? getShortCaliberName(currentAmmo.caliber)
+                                                        : currentAmmo.caliber
+                                                    : currentAmmo[item.name]
+                                                : ""}
+                                    </Text>
+                                </View>
+                            )
+                            }
+                        })}
+                        <View style={{flex: 1, flexDirection: "column"}} >
+                            <Text style={{width: "100%", fontSize: 12,}}>{ammoRemarks[language]}</Text>
+                            <Text style={{width: "100%", fontSize: 18, marginBottom: 5, paddingBottom: 5, borderBottomColor: theme.colors.primary, borderBottomWidth: 0.2}}>{currentAmmo.remarks}</Text>
+                        </View>
+                    </View>
+                    
+                    <Portal>
+                        <Modal visible={lightBoxOpen} onDismiss={setLightBoxOpen}>
+                            <View style={{width: "100%", height: "100%", padding: 0, display: "flex", flexDirection: "row", flexWrap: "wrap", backgroundColor: "green"}}>
+                            <View style={{padding: 0, margin: 0, position: "absolute", top: defaultViewPadding, right: defaultViewPadding, left: defaultViewPadding, zIndex: 999, display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                                    <Pressable onPress={()=>handleShareImage(currentAmmo.images[lightBoxIndex])}><Icon source="share-variant" size={40} color={theme.colors.inverseSurface}/></Pressable>
+                                    <Pressable onPress={setLightBoxOpen} ><Icon source="close-thick" size={40} color={theme.colors.inverseSurface}/></Pressable>
+                                </View>
+                                {lightBoxOpen ? <ImageViewer isLightBox={true} selectedImage={currentAmmo.images[lightBoxIndex]}/> : null}
+                            </View>
+                        </Modal>    
+                    </Portal>   
+
+                    <Portal>
+                        <Dialog visible={dialogVisible} onDismiss={()=>toggleDialogVisible(!dialogVisible)}>
+                            <Dialog.Title>
+                            {`${currentAmmo.designation} ${ammoDeleteAlert.title[language]}`}
+                            </Dialog.Title>
+                            <Dialog.Content>
+                                <Text>{`${ammoDeleteAlert.subtitle[language]}`}</Text>
+                            </Dialog.Content>
+                            <Dialog.Actions>
+                                <Button onPress={()=>deleteItem(currentAmmo)} icon="delete" buttonColor={theme.colors.errorContainer} textColor={theme.colors.onErrorContainer}>{ammoDeleteAlert.yes[language]}</Button>
+                                <Button onPress={()=>toggleDialogVisible(!dialogVisible)} icon="cancel" buttonColor={theme.colors.secondary} textColor={theme.colors.onSecondary}>{ammoDeleteAlert.no[language]}</Button>
+                            </Dialog.Actions>
+                        </Dialog>
+                    </Portal>               
+                    
+                    <View style={{width: "100%", display: "flex", flex: 1, flexDirection: "row", justifyContent:"center"}}>
+                        <Button mode="contained" style={{width: "20%", backgroundColor: theme.colors.errorContainer, marginTop: 20}} onPress={()=>toggleDialogVisible(!dialogVisible)}>
+                            <Icon source="delete" size={20} color={theme.colors.onErrorContainer}/>
+                        </Button>
+                    </View>
+                </ScrollView>
+            </View>
+            
+            <Dialog visible={iosWarning} onDismiss={()=>toggleiosWarning(false)}>
+                    <Dialog.Title>
+                    {iosWarningText.title[language]}
+                    </Dialog.Title>
+                    <Dialog.Content>
+                        <Text>{iosWarningText.text[language]}</Text>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button onPress={()=>handlePrintPress()} icon="heart" buttonColor={theme.colors.errorContainer} textColor={theme.colors.onErrorContainer}>{iosWarningText.ok[language]}</Button>
+                        <Button onPress={()=>toggleiosWarning(false)} icon="heart-broken" buttonColor={theme.colors.secondary} textColor={theme.colors.onSecondary}>{iosWarningText.cancel[language]}</Button>
+                    </Dialog.Actions>
+                </Dialog>
+
+        </View>
+    )
+}
