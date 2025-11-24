cfe41be4a7960dcdb93ea3a0babd59246bc759b8
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-11-24 18:50:45 +0000

    Set Current Accessory

--- stores/useItemStore.ts
+++ stores/useItemStore.ts
@@ -6,11 +6,19 @@ interface ItemStore {
     setCurrentItem: (item: ItemType) => void
     currentCollection: CollectionType
     setCurrentCollection: (collection: CollectionType) => void
+    nameFromId: string | null
+    setNameFromId: (id: string) => void
+    currentAccessory: ItemType | null
+    setCurrentAccessory: (item: ItemType) => void
   }
 
   export const useItemStore = create<ItemStore>((set) => ({
     currentItem: null,
     setCurrentItem: (item: ItemType) => set((state) => ({currentItem: item})),
     currentCollection: "gunCollection",
-    setCurrentCollection: (collection: CollectionType) => set((state) => ({currentCollection: collection}))
+    setCurrentCollection: (collection: CollectionType) => set((state) => ({currentCollection: collection})),
+    nameFromId: null,
+    setNameFromId: (id: string) => set((state) => ({nameFromId: id})),
+    currentAccessory: null,
+    setCurrentAccessory: (item: ItemType) => set((state) => ({currentAccessory: item})),
   }))
