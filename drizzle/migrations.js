ef7eb0cf94e59fe3d68d5c43d7068897eddf0a71
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-11-29 04:20:44 +0000

    Migrations

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -7,6 +7,7 @@ import m0002 from './0002_windy_scalphunter.sql';
 import m0003 from './0003_flippant_captain_midlands.sql';
 import m0004 from './0004_old_blade.sql';
 import m0005 from './0005_chubby_sprite.sql';
+import m0006 from './0006_classy_golden_guardian.sql';
 
   export default {
     journal,
@@ -16,7 +17,8 @@ m0001,
 m0002,
 m0003,
 m0004,
-m0005
+m0005,
+m0006
     }
   }
   
