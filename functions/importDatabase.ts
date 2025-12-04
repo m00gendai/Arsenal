cc751a3388ecdb4d9c143ebe770f942c237b65ab
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-12-04 13:58:43 +0000

    Refactored for collectionType agnostics and additional nonCollection tables

--- functions/importDatabase.ts
+++ functions/importDatabase.ts
@@ -1,13 +1,14 @@
 import * as FileSystem from "expo-file-system";
 import * as DocumentPicker from 'expo-document-picker';
-import { zip, unzip, unzipAssets, subscribe } from 'react-native-zip-archive'
+import { unzip } from 'react-native-zip-archive'
 import * as SQLite from 'expo-sqlite';
-import { drizzle, ExpoSQLiteDatabase } from 'drizzle-orm/expo-sqlite';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
 import * as schema from "../db/schema"
 import { db } from "../db/client"
-import { DB_NAME, ZIP_NAME } from "../configs_DB";
-import { GunType } from "../interfaces";
-import { eq, lt, gte, ne, and, or, like, asc, desc, exists, isNull, sql } from 'drizzle-orm';
+import { DB_NAME } from "../configs_DB";
+import { collectionExportDirectories, collectionImportTables, nonCollectionTables } from "configs";
+import { determineTagSchema } from "./determinators";
+import { CollectionType } from "interfaces";
 
 export default async function importDatabase() {
   try {
@@ -47,49 +48,39 @@ export default async function importDatabase() {
       const cachedDB = SQLite.openDatabaseSync(tempDbName)
       const cacheDBopen = drizzle(cachedDB, { schema })
       
-      // Handle Gun Collection
-      try{
-        const collection = cacheDBopen.select().from(schema.gunCollection).all()
-        const tags = cacheDBopen.select().from(schema.gunTags).all()
-        
+      // Handle Collections
+      collectionImportTables.forEach(async directory =>{
         try{
-          await db.delete(schema.gunCollection)
-          await db.delete(schema.gunTags)
+          const collection = cacheDBopen.select().from(schema[directory]).all()
+          let tags
+          if(!nonCollectionTables.includes(directory)){
+            const tagDirectory = directory as CollectionType
+            tags = cacheDBopen.select().from(determineTagSchema(tagDirectory)).all()
+          }
+          
+          try{
+            await db.delete(schema[directory])
+            if(!nonCollectionTables.includes(directory)){
+              const tagDirectory = directory as CollectionType
+              await db.delete(determineTagSchema(tagDirectory))
+            }
+          }catch(e){
+            throw new Error(`itemCollection ${directory}: DB Delete failed ${e}`)
+          }
+          
+          for(const item of collection){
+            await db.insert(schema[directory]).values(item);
+          }
+          for(const item of tags){
+            if(!nonCollectionTables.includes(directory)){
+              const tagDirectory = directory as CollectionType
+              await db.insert(determineTagSchema(tagDirectory)).values(item);
+            }
+          }
         }catch(e){
-          throw new Error(`gunCollection: DB Delete failed ${e}`)
+          throw new Error(`itemCollection ${directory}: ${e}`)
         }
-        
-        for(const item of collection){
-          await db.insert(schema.gunCollection).values(item);
-        }
-        for(const item of tags){
-          await db.insert(schema.gunTags).values(item);
-        }
-      }catch(e){
-        throw new Error(`gunCollection: ${e}`)
-      }
-      
-      // Handle Ammo Collection
-      try{
-        const collection = cacheDBopen.select().from(schema.ammoCollection).all()
-        const tags = cacheDBopen.select().from(schema.ammoTags).all()
-        
-        try{
-          await db.delete(schema.ammoCollection);
-          await db.delete(schema.ammoTags)
-        }catch(e){
-          throw new Error(`ammoCollection: DB Delete failed ${e}`)
-        }
-        
-        for(const item of collection){
-          await db.insert(schema.ammoCollection).values(item);
-        }
-        for(const item of tags){
-          await db.insert(schema.ammoTags).values(item);
-        }
-      }catch(e){
-        throw new Error(`ammoCollection: ${e}`)
-      }
+      })
       
       // Close and cleanup temp database
       cacheDB.closeSync()
