fca6ae60a3e44107f58907d67b5288e2057873cc
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-12-05 18:35:18 +0000

    New Part Barrel, refactored for mapping over screenNameParams

--- components/BottomBars/BottomBar_PartCollection.tsx
+++ components/BottomBars/BottomBar_PartCollection.tsx
@@ -1,11 +1,11 @@
 import { TouchableOpacity, View } from "react-native";
 import { Divider, Icon, Text } from "react-native-paper";
-import { defaultViewPadding } from "configs";
-import { useViewStore } from "stores/useViewStore";
+import { defaultViewPadding, screenNameParamsPart } from "configs";
 import { usePreferenceStore } from "stores/usePreferenceStore";
-import { mainCollectionCategories, tabBarLabels } from "lib/textTemplates";
+import { mainCollectionCategories } from "lib/textTemplates";
 import { CollectionType } from "interfaces";
 import { useItemStore } from "stores/useItemStore";
+import { determineAccessoryIcons, determineTabBarLabel } from "functions/determinators";
 
 interface Props{
     handleNavigation:(target: "itemCollection", params:{ collectionType: CollectionType })=>void
@@ -35,16 +35,36 @@ export default function BottomBar_PartCollection({handleNavigation}:Props){
             <Text variant={"headlineSmall"} style={{width: "100%", textAlign: "center"}}>
                 {mainCollectionCategories.partCollection[language]}
             </Text>
-           <Divider style={{height: 2, width: "100%", backgroundColor: theme.colors.primary}} />
-            <TouchableOpacity 
-                onPress={()=> handleNavigation("itemCollection", {collectionType: "partCollection_ConversionKit"})} 
-                style={{width: "30%", alignItems: 'center'}}
-            >
-              <Icon source="cog-transfer-outline" size={48} color={currentCollection === "partCollection_ConversionKit" ? theme.colors.primary : theme.colors.secondary} />
-              <Text style={{textAlign: "center", color: currentCollection === "partCollection_ConversionKit" ? theme.colors.primary : theme.colors.secondary, marginTop: 4 }}>
-                {`${tabBarLabels.conversionCollection[language]}`}
-            </Text>
-            </TouchableOpacity>
+
+            <Divider style={{height: 2, width: "100%", backgroundColor: theme.colors.primary}} />
+
+            {screenNameParamsPart.map((collection, index) => {
+                return(
+                    <TouchableOpacity 
+                    key={`${collection}_${index}`}
+                        onPress={()=>
+                            handleNavigation("itemCollection", {collectionType: collection})} 
+                        style={{
+                            width: "30%", 
+                            alignItems: 'center'}}
+                    >
+                        <Icon 
+                            source={determineAccessoryIcons(collection)}
+                            size={48} 
+                            color={currentCollection === collection ? theme.colors.primary : theme.colors.secondary} 
+                        />
+                        <Text 
+                            style={{
+                                textAlign: "center", 
+                                color: currentCollection === collection ? theme.colors.primary : theme.colors.secondary, 
+                                marginTop: 4 }}
+                        >
+                            {`${determineTabBarLabel(collection)[language]}`}
+                        </Text>
+                    </TouchableOpacity>
+                )
+            })}
+
         </View>
     )
 }
