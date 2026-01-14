6fc9f3e541dc2120e54a64c092a888d533658c2f
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-01-14 13:30:08 +0000

    Moved Color Palette to here, added Display Settings

--- components/MainMenu/Settings/Settings.tsx
+++ components/MainMenu/Settings/Settings.tsx
@@ -3,6 +3,8 @@ import { List } from "react-native-paper";
 import { usePreferenceStore } from "stores/usePreferenceStore";
 import Settings_General from "./Settings_General";
 import Settings_Units from "./Settings_Units";
+import Settings_Display from "./Settings_Display";
+import ColorPalette from "./ColorPalette";
 
 export default function Settings(){
 
@@ -10,7 +12,12 @@ export default function Settings(){
 
     return(
         <List.Accordion id="Settings" left={props => <List.Icon {...props} icon="cog-outline" />} title={preferenceTitles.settings[language]} titleStyle={{fontWeight: "700", color: theme.colors.onBackground}}>
+            
+            <ColorPalette />
+            
             <Settings_General />
+
+            <Settings_Display />
         
             <Settings_Units />
         
