23680232f87fe83df7b1e7023727c6680109b366
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-01-14 13:29:48 +0000

    Divided Settings into General and Display

--- components/MainMenu/Settings/Settings_Display.tsx
+++ components/MainMenu/Settings/Settings_Display.tsx
@@ -1,19 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { defaultViewPadding } from "configs";
 import { PREFERENCES } from "configs_DB";
-import { generalSettingsLabels, loginGuardAlert, preferenceTitles, resizeImageAlert } from "lib/textTemplates";
+import { displaySettingsLabels, loginGuardAlert, preferenceTitles, resizeImageAlert } from "lib/textTemplates";
 import { View } from "react-native";
 import { Dialog, Divider, List, Switch, Text, Button } from "react-native-paper";
 import { usePreferenceStore } from "stores/usePreferenceStore";
 import { useViewStore } from "stores/useViewStore"
-import * as LocalAuthentication from 'expo-local-authentication';
 
-export default function Settings_General(){
+export default function Settings_Display(){
 
     const { theme, language, generalSettings, setGeneralSettings } = usePreferenceStore()
     const { imageResizeVisible, toggleImageResizeVisible, loginGuardVisible, toggleLoginGuardVisible } = useViewStore()
 
     async function handleSwitches(setting: string){
+        // DISPLAY SETTINGS ARE STORED AS GENERAL SETTINGS, the distinction is only made for UI reasons
         const newSettings = {...generalSettings, [setting]: !generalSettings[setting]}
         setGeneralSettings(newSettings)
         const preferences:string = await AsyncStorage.getItem(PREFERENCES)
@@ -21,52 +21,42 @@ export default function Settings_General(){
         await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
     }
 
-    async function handleSwitchesAlert(setting:string){
-        if(setting === "resizeImages"){
-            toggleImageResizeVisible()        
-        }
-        if(setting === "loginGuard"){
-            const compatible = await LocalAuthentication.hasHardwareAsync();
-            console.info(`compatible: ${compatible}`)
-            const isEnrolled = await LocalAuthentication.isEnrolledAsync()
-            console.info(`isEnrolled: ${isEnrolled}`)
-            const getEnrolledLevel = await LocalAuthentication.getEnrolledLevelAsync()
-            console.info(`getEnrolledLevel: ${getEnrolledLevel}`)
-            if(!compatible){
-                toggleLoginGuardVisible()
-            }
-            if(!isEnrolled){
-                toggleLoginGuardVisible()
-            }
-            if(compatible && isEnrolled){
-                handleSwitches("loginGuard")
-            }
-        }
-    }
-
     return( 
         <View style={{backgroundColor: theme.colors.tertiaryContainer}}>
-            <List.Accordion left={props => <List.Icon {...props} icon="tune" />} title={preferenceTitles.generalSettings[language]} titleStyle={{fontWeight: "700", color: theme.colors.onTertiaryContainer}} style={{paddingLeft: defaultViewPadding*2, backgroundColor: theme.colors.tertiaryContainer}}>
+            <List.Accordion left={props => <List.Icon {...props} icon="monitor-dashboard" />} title={preferenceTitles.displaySettings[language]} titleStyle={{fontWeight: "700", color: theme.colors.onTertiaryContainer}} style={{paddingLeft: defaultViewPadding*2, backgroundColor: theme.colors.tertiaryContainer}}>
                 <View style={{ marginLeft: 5, marginRight: 5, padding: defaultViewPadding, backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.primary, borderLeftWidth: 5}}>
                     <View style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", flexWrap: "wrap", gap: 5}}>
-                        
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
-                            <Text style={{flex: 7}}>{generalSettingsLabels.resizeImages[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.resizeImages} onValueChange={()=>generalSettings.resizeImages ? handleSwitchesAlert("resizeImages") : handleSwitches("resizeImages")} />
+                            <Text style={{flex: 7}}>{displaySettingsLabels.displayImagesInListViewGun[language]}</Text>
+                            <Switch style={{flex: 3}} value={generalSettings.displayImagesInListViewGun} onValueChange={()=>handleSwitches("displayImagesInListViewGun")} />
                         </View>
                         
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
-                            <Text style={{flex: 7}}>{generalSettingsLabels.loginGuard[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.loginGuard} onValueChange={()=>handleSwitchesAlert("loginGuard")} />
+                            <Text style={{flex: 7}}>{displaySettingsLabels.displayImagesInListViewAmmo[language]}</Text>
+                            <Switch style={{flex: 3}} value={generalSettings.displayImagesInListViewAmmo} onValueChange={()=>handleSwitches("displayImagesInListViewAmmo")} />
                         </View>
-
+                        
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
-                            <Text style={{flex: 7}}>{generalSettingsLabels.hintsDisplay[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.hintsDisplay} onValueChange={()=>handleSwitches("hintsDisplay")} />
+                            <Text style={{flex: 7}}>{displaySettingsLabels.titleBelowImage[language]}</Text>
+                            <Switch style={{flex: 3}} value={generalSettings.titleBelowImage} onValueChange={()=>handleSwitches("titleBelowImage")} />
+                        </View>
+                        
+                        <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
+                        
+                        <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
+                            <Text style={{flex: 7}}>{displaySettingsLabels.emptyFields[language]}</Text>
+                            <Switch style={{flex: 3}} value={generalSettings.emptyFields} onValueChange={()=>handleSwitches("emptyFields")} />
+                        </View>
+                        
+                        <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
+                        
+                        <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
+                            <Text style={{flex: 7}}>{displaySettingsLabels.caliberDisplayName[language]}</Text>
+                            <Switch style={{flex: 3}} value={generalSettings.caliberDisplayName} onValueChange={()=>handleSwitches("caliberDisplayName")} />
                         </View>
                         
                     </View>
