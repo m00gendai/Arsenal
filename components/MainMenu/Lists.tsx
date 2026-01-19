8737c0f3223f59c84ebfb17a4c292f191c6e9266
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-01-19 18:28:32 +0000

    New print generation logic

--- components/MainMenu/Lists.tsx
+++ components/MainMenu/Lists.tsx
@@ -1,46 +1,45 @@
 import { defaultViewPadding } from "configs/configs";
-import { printGunCollection, printGunCollectionArt5 } from "functions/printers/printToPDF";
+import { printGunCollection } from "functions/printers/printGunCollectionToPDF";
 import { iosWarningText, preferenceTitles } from "lib/textTemplates";
 import { useState } from "react";
 import { Platform, View } from "react-native";
 import { Button, Dialog, Divider, IconButton, List, Text } from "react-native-paper";
 import { usePreferenceStore } from "stores/usePreferenceStore";
 import { alarm } from "functions/utils";
+import { ListPrinter } from "lib/interfaces";
+import { determineAccessoryIcons } from "functions/determinators";
+import { printAmmoCollection } from "functions/printers/printAmmoCollectionToPDF";
 
 export default function Lists(){
 
-    const { language, theme, generalSettings, caliberDisplayNameList } = usePreferenceStore()
+    const { language, theme, generalSettings, caliberDisplayNameList, preferredUnits } = usePreferenceStore()
 
-    const [printerSrc, setPrinterSrc] = useState<null | "gunCollection" | "gunCollectionArt5" | "ammoCollection">(null)
+    const [printerSrc, setPrinterSrc] = useState<ListPrinter>(null)
     const [iosWarning, toggleiosWarning] = useState<boolean>(false)
 
-    async function handlePrints(printer: null | "gunCollection" | "gunCollectionArt5" | "ammoCollection"){
+    async function handlePrints(printer: ListPrinter){
         if(printer === null){
             return
         }
         toggleiosWarning(false)
-        switch(printer){
-            case "gunCollection":
-                try{
-                await printGunCollection(language, generalSettings.caliberDisplayName, caliberDisplayNameList);
-               
-                } catch(e){
-                    alarm("printGunCollection Error", e)
-                }
-                break
-            case "gunCollectionArt5":
-                try{
-                   await printGunCollectionArt5(language, generalSettings.caliberDisplayName, caliberDisplayNameList);
-                 
-                } catch(e){
-                    alarm("printGunCollectioNArt5 Error", e)
-                }
-                break
+        if(printer.startsWith("gunCollection")){
+            try {
+                await printGunCollection(language, generalSettings.caliberDisplayName, caliberDisplayNameList, printer, preferredUnits)
+            }catch(e){
+                console.error(`Print Gun Collection Error: ${e}`)
+            }
+        }
+        if(printer.startsWith("ammoCollection")){
+            try{
+                await printAmmoCollection(language, generalSettings.caliberDisplayName, caliberDisplayNameList, printer, preferredUnits)
+            }catch(e){
+                console.error(`Print Ammo Collection Error: ${e}`)
+            }
         }
     }
 
 
-    async function handleIOSprints(printer: "gunCollection" | "gunCollectionArt5" | "ammoCollection"){
+    async function handleIOSprints(printer: ListPrinter){
         setPrinterSrc(printer)
         toggleiosWarning(true)
     }
@@ -51,15 +50,32 @@ export default function Lists(){
             <List.Accordion left={props => <List.Icon {...props} icon="printer" />} title={preferenceTitles.gunList[language]} titleStyle={{fontWeight: "700", color: theme.colors.onBackground}}>
                 <View style={{ marginLeft: 5, marginRight: 5, padding: defaultViewPadding, backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.primary, borderLeftWidth: 5}}>
                     <View style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", flexWrap: "wrap", gap: 5}}>
+                        
                         <View style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%"}}>
                             <Text style={{width: "80%"}}>{preferenceTitles.printAllGuns[language]}</Text>
-                            <IconButton icon="table-large" onPress={()=>Platform.OS === "ios" ? handleIOSprints("gunCollection") : handlePrints("gunCollection")} mode="contained" iconColor={theme.colors.onPrimary} style={{backgroundColor: theme.colors.primary}}/>
+                            <IconButton icon={determineAccessoryIcons("gunCollection")} onPress={()=>Platform.OS === "ios" ? handleIOSprints("gunCollection") : handlePrints("gunCollection")} mode="contained" iconColor={theme.colors.onPrimary} style={{backgroundColor: theme.colors.primary}}/>
                         </View>   
+                        
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
+                        
                         <View style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%"}}>
                             <Text style={{width: "80%"}}>{preferenceTitles.printArt5[language]}</Text>
-                            <IconButton icon="table-large" onPress={()=>Platform.OS === "ios" ? handleIOSprints("gunCollectionArt5") : handlePrints("gunCollectionArt5")} mode="contained" iconColor={theme.colors.onPrimary} style={{backgroundColor: theme.colors.primary}}/>
+                            <IconButton icon={determineAccessoryIcons("gunCollection")} onPress={()=>Platform.OS === "ios" ? handleIOSprints("gunCollectionArt5") : handlePrints("gunCollectionArt5")} mode="contained" iconColor={theme.colors.onPrimary} style={{backgroundColor: theme.colors.primary}}/>
                         </View>   
+
+                        <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
+                        
+                        <View style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%"}}>
+                            <Text style={{width: "80%"}}>{preferenceTitles.printGunsHybrid[language]}</Text>
+                            <IconButton icon={determineAccessoryIcons("gunCollection")} onPress={()=>Platform.OS === "ios" ? handleIOSprints("gunCollectionHybrid") : handlePrints("gunCollectionHybrid")} mode="contained" iconColor={theme.colors.onPrimary} style={{backgroundColor: theme.colors.primary}}/>
+                        </View> 
+
+                        <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
+                        
+                        <View style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%"}}>
+                            <Text style={{width: "80%"}}>{preferenceTitles.printAllAmmo[language]}</Text>
+                            <IconButton icon={determineAccessoryIcons("ammoCollection")} onPress={()=>Platform.OS === "ios" ? handleIOSprints("ammoCollection") : handlePrints("ammoCollection")} mode="contained" iconColor={theme.colors.onPrimary} style={{backgroundColor: theme.colors.primary}}/>
+                        </View>
                         
                     </View>
                 </View>
