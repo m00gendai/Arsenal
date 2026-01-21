c24b12df1f811de2bff4efcd0993e894fa72a557
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-01-21 09:41:24 +0000

    Refactored print generation logic, outsourced styles, refactored footer

--- functions/printers/printSingleItem.tsx
+++ functions/printers/printSingleItem.tsx
@@ -4,11 +4,13 @@ import { dateLocales, datePickerTriggerFields, dateTimeOptions, legacyDatePicker
 import { checkBoxes, gunRemarks } from "lib/DataTemplates/gunDataTemplate";
 import { determineDataTemplate } from "functions/determinators";
 import { newTags, pdfFooter } from "lib/textTemplates";
-import { getShortCaliberNameFromArray } from "functions/utils";
+import { getShortCaliberNameFromArray, parseDate } from "functions/utils";
 import { Platform } from "react-native";
 import * as Print from 'expo-print';
 import { shareAsync } from "expo-sharing";
 import * as IntentLauncher from 'expo-intent-launcher';
+import { galleryStyle } from "./printoutStyles";
+import * as Application from 'expo-application';
 
 
 export async function printSingleItem(item:ItemType, collection: CollectionType, language: string, shortCaliber: boolean, caliberDisplayNameList: {name:string, displayName?:string}[]){
@@ -46,18 +48,8 @@ export async function printSingleItem(item:ItemType, collection: CollectionType,
         return data ? data[language] : remarks ? remarks : boxes ? boxes[language] : tags ? tags : key;
     }
 
-    function parseDate(date: string){
-        if(date === null){
-            return ""
-        }
-        return new Date(date).toLocaleDateString("de-CH", dateTimeOptions)
-    }
-
     const html = `
         <html>
-            <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
-            </head>
             <body>
                 <div class="bodyContent">
                     <h1>${getTitle(item)}</h1>
@@ -82,136 +74,21 @@ export async function printSingleItem(item:ItemType, collection: CollectionType,
                                     </td>
                                 </tr>`
                             }).join("")}
+                            <tr>
+                            <td colspan="2"><strong>${gunRemarks[language]}</td>
+                            </tr>
+                            <tr>
+                            <td colspan="2">${item.remarks}</td>
+                            </tr>
+
                         </tbody>
+                        <tfoot><tr><td colspan="2">${getTitle(item)}: ${pdfFooter[language].replace("{{{A}}}", Application.applicationName).replace("{{{B}}}", Platform.OS)} ${Application.nativeApplicationVersion}, ${generatedDate}</td></tr></tfoot>
                     </table>
-                    ${item.remarks && item.remarks.length !== 0 ? `<div class="remarkContainer"><div class="remarkContainerTitle"><strong>${gunRemarks[language]}</strong></div><div class="remarkContainerContent">${item.remarks}</div></div>`: ""}
                 </div>
             </body>
-            <div class="footer">${getTitle(item)}: ${pdfFooter[language]}, ${generatedDate}</div>
             
             <style>
-                @page {
-                    size: A4;
-                    margin: ${pdfCommonStyles.allPageMargin};
-                }
-                body{
-                    display: flex;
-                    justify-content: center;
-                    align-items: flex-start;
-                    align-content: flex-start;
-                    margin: 0;
-                    padding: 0;
-                    font-family: "Helvetica";
-                }
-                h1{
-                    position: relative;
-                    width: 100%;
-                    text-align: left;
-                    margin: 0;
-                    padding: 0;
-                    font-size: ${pdfCommonStyles.allTitleFontSize};
-                }
-                hr{
-                    width: 100%;
-                }
-                .bodyContent{
-                    width: 100%;
-                    padding: 0;
-                    box-sizing: border-box;
-                }
-                .imageContainer {
-                    position: relative;
-                    width: 100%;
-                    aspect-ratio: 30/10;
-                    display: flex;
-                    gap: ${pdfCommonStyles.imageGap};
-                    justify-content: center;
-                    align-items: center;
-                    flex-wrap: nowrap;
-                    margin: 10px 0;
-                    box-shadow: 0px 2px 5px -2px black;
-                    padding: 5px;
-                }
-                .imageDiv {
-                    position: relative;
-                    width: 100%;
-                    height: 100%;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-                .image {
-                    max-width: 100%;
-                    max-height: 100%;
-                    object-fit: contain;
-                }
-                .tagContainer{
-                    position: relative;
-                    width: 100%;
-                    display: flex;
-                    justify-content: flex-start;
-                    align-items: flex-start; 
-                    flex-wrap: wrap;
-                    gap: ${pdfCommonStyles.tagContainerGap};
-                }
-                .tag{
-                    position: relative;
-                    border: 1px solid black;
-                    padding: ${pdfCommonStyles.tagPadding};
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    font-size: ${pdfCommonStyles.tagFontSize};
-                }
-                table {
-                    position: relative;
-                    margin: ${pdfCommonStyles.tableVerticalMargin} 0;
-                    width: 100%;
-                    font-size: ${pdfCommonStyles.allTableFontSize};
-                    border-collapse: collapse;
-                }
-                table > tbody > tr {
-                    padding: ${pdfCommonStyles.tableRowVerticalPadding} 0;
-                }
-                table > tbody > tr:nth-child(even){
-                    background-color: #f5f5f5;
-                }
-                table > tbody > tr > td {
-                    padding: ${pdfCommonStyles.tableCellPadding};
-                }
-                .remarkContainer{
-                    position: relative;
-                    width: 100%;
-                    display: flex;
-                    justify-content: center;
-                    align-items: flex-start;
-                    flex-wrap: wrap;
-                }
-                .remarkContainerTitle{
-                    position: relative;
-                    width: 100%;
-                    display: flex;
-                    justify-content: flex-start;
-                    align-items: center;
-                }
-                .remarkContainerContent{
-                    position: relative;
-                    width: 100%;
-                    white-space: pre-wrap;
-                }
-                .footer{
-                    position: fixed;
-                    bottom: 0;
-                    width: ${pdfCommonStyles.footerWidth};
-                    font-size: ${pdfCommonStyles.footerFontSize};
-                    border-top: ${pdfCommonStyles.footerTopBorder};
-                    padding-top: ${pdfCommonStyles.footerPaddingTop};
-                    margin-top: ${pdfCommonStyles.footerMarginTop};
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    align-content: center;
-                }
+                ${galleryStyle}
             </style>
         </html>
     `
