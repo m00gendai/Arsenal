2a4edf9cae10d25e08b5f9f5588a8a4dbbbc4d56
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-01-25 08:45:27 +0000

    Injecting bad data added, moved importLegacyDatabaseAsJSON.ts to DEV function folder

---
+++ functions/DEV/DEV_injectBadItem_date.ts
@@ -0,0 +1,17 @@
+import { emptyAmmoObject } from "lib/DataTemplates/ammoDataTemplate";
+import * as schema from "db/schema"
+import { db } from "db/client"
+import { AmmoType, GunType } from "lib/interfaces";
+import { v4 as uuidv4 } from 'uuid';
+import { emptyGunObject } from "lib/DataTemplates/gunDataTemplate";
+
+export default async function DEV_injectBadItem_date(){
+    const badItem: GunType = {...emptyGunObject}
+    try{
+        // This injects deliberately bad data so errors are expected
+        /*@ts-expect-error */
+        await db.insert(schema.gunCollection).values({...badItem, id: uuidv4(), acquisitionDate_unix: "b"})
+    } catch(e){
+        console.error(e)
+    }
+}
