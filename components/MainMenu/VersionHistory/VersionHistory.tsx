7f9839a0e2c998f09482dd3a79ccb6e9ebe35f13
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-01-25 12:17:07 +0000

    Fixed crash if Rumantsch is selected (release notes do not have Rumantsch language)

--- components/MainMenu/VersionHistory/VersionHistory.tsx
+++ components/MainMenu/VersionHistory/VersionHistory.tsx
@@ -4,6 +4,7 @@ import { preferenceTitles } from "lib/textTemplates";
 import { defaultViewPadding } from "configs/configs";
 import { usePreferenceStore } from "stores/usePreferenceStore";
 import { versionHistory } from "./releaseNotes";
+import { Languages } from "lib/interfaces";
 
 
 export default function VersionHistory(){
@@ -15,14 +16,15 @@ export default function VersionHistory(){
             <List.Accordion left={props => <List.Icon {...props} icon="clock-start" />} title={preferenceTitles.versionHistory[language]} titleStyle={{fontWeight: "700", color: theme.colors.onBackground}}>
                 <View style={{ marginLeft: 5, marginRight: 5, padding: defaultViewPadding, backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.primary, borderLeftWidth: 5}}>
                     {versionHistory.map((version, index) =>{
+                        const lang:Languages = language === "ch" ? "de" : language
                         return(
                             <View 
                                 key={`VersionHistory_${index}`}
                             >
                                 <Text style={{fontWeight: "bold"}}>{`${version.title}`}</Text>
-                                <Text>{`${version[language].text}`}</Text>
-                                {Platform.OS === "ios" && version[language].ios ? <View><Text style={{fontStyle: "italic"}}>{`\niOS:`}</Text><Text>{version[language].ios}</Text></View> : null}
-                                {Platform.OS === "android" && version[language].android ? <View><Text style={{fontStyle: "italic"}}>{`\nAndroid:`}</Text><Text>{version[language].android}</Text></View> : null}
+                                <Text>{`${version[lang].text}`}</Text>
+                                {Platform.OS === "ios" && version[lang].ios ? <View><Text style={{fontStyle: "italic"}}>{`\niOS:`}</Text><Text>{version[lang].ios}</Text></View> : null}
+                                {Platform.OS === "android" && version[lang].android ? <View><Text style={{fontStyle: "italic"}}>{`\nAndroid:`}</Text><Text>{version[lang].android}</Text></View> : null}
                                  <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.backdrop, marginTop: defaultViewPadding, marginBottom: defaultViewPadding}} />
                             </View>
                         )
