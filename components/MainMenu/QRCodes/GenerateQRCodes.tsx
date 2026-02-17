3dc27daa42ab906908bb76326066bce4ffa6c334
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-02-17 17:32:46 +0000

    Component to design Shipping Labels

---
+++ components/MainMenu/QRCodes/GenerateQRCodes.tsx
@@ -0,0 +1,163 @@
+import { Dimensions, FlatList, TouchableNativeFeedback, View } from "react-native"
+import { Appbar, Button, Checkbox, Divider, Icon, IconButton, List, RadioButton, Switch, Text } from "react-native-paper"
+import { usePreferenceStore } from "stores/usePreferenceStore"
+import * as schema from "db/schema"
+import { db } from "db/client"
+import { eq, asc, inArray } from 'drizzle-orm';
+import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
+import { defaultViewPadding } from "configs/configs"
+import { ScrollView } from "react-native-gesture-handler"
+import { dataTemplate_Translations } from "lib/DataTemplates/translations"
+import { preferenceTitles, tabBarLabels } from "lib/textTemplates"
+import { CollectionType, ItemType } from "lib/interfaces"
+import { useRoute } from "@react-navigation/native"
+import { Dropdown } from "react-native-paper-dropdown"
+import { LabelTemplate, shippingLabelData } from "lib/shippingLables"
+import { useCallback, useState } from "react"
+import QRCodeStyled from 'react-native-qrcode-styled';
+import QRCode from 'react-native-qrcode-skia';
+
+interface RouteParams {
+  collection: CollectionType  
+}
+
+export default function GenerateQRCodes({navigation}){
+
+    const route = useRoute()
+    const params = route.params as RouteParams
+
+    const { theme, language } = usePreferenceStore()
+
+    const [contentIndex, setContentIndex] = useState<number>(0)
+    const [selectedLabel, setSelectedLabel] = useState<string>(shippingLabelData[0].id)
+    const [selectedGuns, setSelectedGuns] = useState(new Set<string>());
+    const [qrCodeEnabled, setQrCodeEnabled] = useState<boolean>(true)
+    const [textEnabled, setTextEnabled] = useState<boolean>(true)
+    const [fontSize, setFontSize] = useState<number>(1)
+
+    const collectionItems = db.select().from(schema[params.collection]).all()
+
+    const labelOptions = shippingLabelData.map(label => {
+        return {label: `${label.region}: ${label.name} ${label.labelWidth}x${label.labelHeight}${label.unit}`, value: label.id}
+    })
+
+    const titles = [
+        "Select Label Format", "Select Items", "Adjust Label"
+    ]
+
+    const handleCheckboxPress = useCallback((id: string) => {
+        setSelectedGuns(prev => {
+            const newSet = new Set(prev);
+            if (newSet.has(id)) {
+                newSet.delete(id);
+            } else {
+                newSet.add(id);
+            }
+            return newSet;
+        })
+    }, [])
+
+    function getWidthHeight(){
+        const label:LabelTemplate[] = shippingLabelData.filter(shippingLabel =>{
+            return shippingLabel.id === selectedLabel
+        })
+        console.log({height: label[0].labelHeight, width: label[0].labelWidth})
+
+        const displayWidth = Dimensions.get("window").width-(defaultViewPadding*2)
+        const factor = displayWidth/label[0].labelWidth
+        return {height: label[0].labelHeight*factor, width: label[0].labelWidth*factor}
+    }
+
+    function getSelectedItemsFromDatabase(){
+        const items = collectionItems.filter(item => selectedGuns.has(item.id)) as ItemType[]
+        return items
+    }
+
+    return(
+        <View style={{flex: 1, paddingBottom: defaultViewPadding}}>
+            <Appbar style={{width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                <Appbar.BackAction onPress={() => navigation.goBack()} />
+                <Appbar.Content title={tabBarLabels[params.collection][language]}/>
+            </Appbar>
+            <View><Text variant="titleMedium">{titles[contentIndex]}</Text></View>
+            {contentIndex === 0 ? 
+                <ScrollView style={{flex: 1}}>
+                    <RadioButton.Group onValueChange={value => setSelectedLabel(value)} value={selectedLabel}>
+                    {labelOptions.map((label, index) => {
+                        return <RadioButton.Item key={`label_${index}`} label={label.label} value={label.value} mode="android"/>
+                    })}
+                    </RadioButton.Group>
+                </ScrollView> 
+            :
+            contentIndex === 1 ? 
+                <View style={{flex: 1}}>
+                    <FlatList
+                        data={collectionItems}
+                        keyExtractor={(item, index) => `label_${index}`}
+                        renderItem={({ item }) => (
+                            <Checkbox.Item
+                                label={`${"manufacturer" in item ? item.manufacturer : ""}${"manufacturer" in item ? " " : ""}${item.model}`}
+                                status={selectedGuns.has(item.id) ? "checked" : "unchecked"}
+                                onPress={() => handleCheckboxPress(item.id)}
+                                mode="android"
+                            />
+                        )}
+                    />
+                </View> 
+            :
+            contentIndex === 2 ? 
+                <View style={{flex: 1}}>
+                    <View 
+                        style={{
+                            width: getWidthHeight().width, 
+                            height: getWidthHeight().height, 
+                            borderColor: theme.colors.primary,
+                            borderWidth: 1,
+                            alignSelf: "center",
+                            display: "flex",
+                            flexDirection: "row",
+                            }}
+                        >
+                            {qrCodeEnabled ? <View style={{padding: defaultViewPadding, display: "flex", alignItems: "center", alignContent: "center", flexDirection: "row"}}>
+                                <QRCode
+                                    value="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                                    size={(getWidthHeight().width/3)-10}
+                                />
+                            </View> : null}
+                            {textEnabled ? <View style={{padding: defaultViewPadding, flexDirection: 'column', flex: 1, flexShrink: 1}}>
+                                <View>
+                                    <Text style={{fontSize: fontSize}}>{`${dataTemplate_Translations.manufacturer[language]}:`}</Text>
+                                    <Text style={{fontSize: fontSize, fontWeight: "bold"}} numberOfLines={2} >{`${getSelectedItemsFromDatabase()[0].manufacturer}`}</Text>
+                                </View>
+                                <View>
+                                    <Text style={{fontSize: fontSize}}>{`${dataTemplate_Translations.model[language]}:`}</Text>
+                                    <Text style={{fontSize: fontSize, fontWeight: "bold"}} numberOfLines={2} >{`${getSelectedItemsFromDatabase()[0].model}`}</Text>
+                                </View>
+                                <View>
+                                    <Text style={{fontSize: fontSize}}>{`${dataTemplate_Translations.serial[language]}:`}</Text>
+                                    <Text style={{fontSize: fontSize, fontWeight: "bold"}} numberOfLines={2} >{`${getSelectedItemsFromDatabase()[0].serial}`}</Text>
+                                </View>
+                            </View> : null}
+                        </View>
+                        <View>
+                            <View style={{width: "50%", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                                <Text>With QR Code</Text>
+                                <Switch value={qrCodeEnabled} onValueChange={()=> setQrCodeEnabled(prev => !prev)} />
+                            </View>
+                            <View style={{width: "50%", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                                <Text>With Text</Text>
+                                <Switch value={textEnabled} onValueChange={()=> setTextEnabled(prev => !prev)} />
+                            </View>
+                        </View>
+                </View>
+            :
+            null
+            }
+
+            <View style={{backgroundColor: "red"}}>
+                {contentIndex !== 0 ? <Button onPress={()=> setContentIndex(contentIndex => contentIndex-1)}>Previous</Button> : null}
+                <Button onPress={()=> setContentIndex(contentIndex => contentIndex+1)}>Next</Button>
+            </View>
+        </View>
+    )
+}
