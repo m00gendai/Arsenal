8babfee903c7498d63ab70a35f9128cfc593f504
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-03-14 21:11:00 +0000

    Migrations

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -22,6 +22,7 @@ import m0017 from './0017_naive_peter_quill.sql';
 import m0018 from './0018_flawless_iceman.sql';
 import m0019 from './0019_absent_norman_osborn.sql';
 import m0020 from './0020_sturdy_corsair.sql';
+import m0021 from './0021_wide_celestials.sql';
 
   export default {
     journal,
@@ -46,7 +47,8 @@ m0016,
 m0017,
 m0018,
 m0019,
-m0020
+m0020,
+m0021
     }
   }
   
