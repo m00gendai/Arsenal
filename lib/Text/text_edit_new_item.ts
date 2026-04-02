45daf35d9e9c9ab821617159ce5462fa2e2ef8dc
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-04-02 18:55:12 +0000

    Split Text templates into dedicated thematically grouped files

---
+++ lib/Text/text_edit_new_item.ts
@@ -0,0 +1,81 @@
+import { SimpleTranslation } from "./interfaces_text"
+
+export const editGunTitle:SimpleTranslation = {
+    de: "Waffe bearbeiten",
+    en: "Edit Gun",
+    fr: "Modifier l'arme",
+    it: "Modifica arma",
+    ch: "Elavurar in'arma",
+}
+
+export const newGunTitle:SimpleTranslation = {
+    de: "Neue Waffe",
+    en: "New Gun",
+    fr: "Nouvelle arme",
+    it: "Nuova arma",
+    ch: "Nova arma",
+}
+
+export const editAmmoTitle:SimpleTranslation = {
+    de: "Munition bearbeiten",
+    en: "Edit Ammunition",
+    fr: "Modifier les munitions",
+    it: "Modifica munizioni",
+    ch: "Elavurar muniziun",
+}
+
+export const newAmmoTitle:SimpleTranslation = {
+    de: "Neue Munition",
+    en: "New Ammunition",
+    fr: "Nouvelles munitions",
+    it: "Nuove munizioni",
+    ch: "Nova muniziun",
+}
+
+export const editAccessoryTitle:SimpleTranslation = {
+    de: "Zubehör bearbeiten",
+    en: "Edit Accessory",
+    fr: "Modifier l’accessoire",
+    it: "Modificare accessorio",
+    ch: "Modifitgar accessoris",
+}
+
+export const newAccessoryTitle:SimpleTranslation = {
+    de: "Neues Zubehör",
+    en: "New Accessory",
+    fr: "Nouvel accessoire",
+    it: "Nuovo accessorio",
+    ch: "Nov accessori",
+}
+
+export const editPartTitle:SimpleTranslation = {
+    de: "Waffenbestandteil bearbeiten",
+    en: "Edit Weapon Part",
+    fr: "Modifier la partie d’une arme",
+    it: "Modifica parte arma",
+    ch: "Modifitgar ina part da l’arma",
+}
+
+export const newPartTitle:SimpleTranslation = {
+    de: "Neues Waffenbestandteil",
+    en: "New Weapon Part",
+    fr: "Pièce de nouvelle arme",
+    it: "Nuova parte di arma",
+    ch: "Nova part da las armas",
+}
+
+export const newLiteratureTitle:SimpleTranslation = {
+    de: "Neue Literatur",
+    en: "New Literature",
+    fr: "Nouvelle littérature",
+    it: "Nuova letteratura",
+    ch: "Nova litteratura",
+}
+
+export const editLiteratureTitle:SimpleTranslation = {
+    de: "Literatur bearbeiten",
+    en: "Edit Literature",
+    fr: "Éditer la littérature",
+    it: "Edit Letteratura",
+    ch: "Edì litteratura",
+}
