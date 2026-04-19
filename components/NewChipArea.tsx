e9cd17d56ebffa989b9f800d7d2cfca5daa4b686
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-04-19 11:20:11 +0000

    Layout improvements

--- components/NewChipArea.tsx
+++ components/NewChipArea.tsx
@@ -1,7 +1,7 @@
 import { View, TouchableNativeFeedback, ScrollView } from "react-native"
 import { newTags, tagModal } from "../lib/textTemplates"
 import { usePreferenceStore } from "../stores/usePreferenceStore"
-import { Button, Chip, IconButton, TextInput, Text, Dialog, Surface, Modal, Portal } from "react-native-paper"
+import { Button, Chip, IconButton, TextInput, Text, Dialog, Surface, Modal, Portal, Divider } from "react-native-paper"
 import { ItemType } from "../lib/interfaces"
 import { useState } from "react"
 import { defaultViewPadding } from "../configs/configs"
@@ -80,74 +80,76 @@ export default function NewChipArea({data, itemData, setItemData}:Props){
     }
 
     return(
-        <View >
-        <TouchableNativeFeedback onPress={()=>setViewTagModal(true)} >
-            <View style={{ flexDirection: "row", flexWrap: "wrap", marginBottom: 10, marginTop: 10}}>
-                {itemData.tags && itemData.tags.length !== 0 ?
-                    itemData.tags.map((tag, index) =>{
-                        return  <View key={`${tag}_${index}`} style={{padding: 5}}><Chip >{tag}</Chip></View>
-                }) : <Chip>{newTags[language]}</Chip>}
-            </View>
-        </TouchableNativeFeedback>
-
-        
-        <ModalContainer visible={viewTagModal} setVisible={setViewTagModal}
-            title={tagModal.title[language]}
-            subtitle={tagModal.subtitle[language]}
-            content={<View><Surface style={{display: "flex", flexDirection: "row", flexWrap: "wrap", marginTop: 10, marginBottom: 10, justifyContent: "flex-start", padding: defaultViewPadding}}>
-            <Text style={{width: "100%"}}>{tagModal.existingTags[language]}</Text>
-            <View style={{height: 100, width: "100%"}}>
-                <ScrollView contentContainerStyle={{width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "flex-start"}}>
-                {Array.from(new Set(itemTags.map((tag, index) => 
-                        <View key={`${tag.label}_${index}`} style={{padding: 5}}>
-                            <Chip onPress={()=>addTagFromList(tag.label)} onClose={()=>handleDeleteTagFromList(tag.label)}>{tag.label}</Chip>
-                        </View>)
-                ))}
-                </ScrollView>
+        <View>
+            <TouchableNativeFeedback onPress={()=>setViewTagModal(true)} >
+                <View style={{ flexDirection: "row", flexWrap: "wrap", marginBottom: 10, marginTop: 10}}>
+                    {itemData.tags && itemData.tags.length !== 0 ?
+                        itemData.tags.map((tag, index) =>{
+                            return  <View key={`${tag}_${index}`} style={{padding: 5}}><Chip >{tag}</Chip></View>
+                    }) : <Chip>{newTags[language]}</Chip>}
                 </View>
-            </Surface>
-            <Surface style={{display: "flex", flexDirection: "row", alignItems: "center", marginTop: 10, marginBottom: 10, padding: defaultViewPadding}}>
-                <TextInput
-                    label={tagModal.inputTags[language]}
-                    value={text}
-                    onChangeText={text => setText(text)}
-                    style={{flex: 8, marginRight: 10}}
-                    returnKeyType='done'
-                    returnKeyLabel='OK'
-                    />
-                    <IconButton mode="contained" icon={"floppy"} size={30} onPress={()=>saveNewTag(null)} style={{ margin: 0}} />
-            </Surface>
-            <Surface style={{display: "flex", flexDirection: "row", flexWrap: "wrap", marginTop: 10, marginBottom: 10, justifyContent: "flex-start", padding: defaultViewPadding}}>
-            <Text style={{width: "100%"}}>{tagModal.selectedTags[language]}</Text>
-            <View style={{height: 100, width: "100%"}}>
-                <ScrollView contentContainerStyle={{width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "flex-start"}}>
-                {itemData.tags?.map((tag, index) =>{
-                    return <View key={`${tag}_${index}`} style={{padding: 5}}><Chip onClose={()=>deleteTag(tag)}>{tag}</Chip></View>
-                })}
-                </ScrollView>
-            </View>
-            </Surface>
-            <Dialog visible={tagDeleteDialogVisible} onDismiss={()=>toggleTagDeleteDialogVisible(false)}>
-            <Dialog.Title>
-                    {deleteTagFromListAlert.title[language]}
-                    </Dialog.Title>
-                    <Dialog.Content>
-                        <Text>{deleteTagFromListAlert.subtitle[language]}</Text>
-                    </Dialog.Content>
-                    <Dialog.Actions>
-                        <Button onPress={()=>deleteTagFromList()} icon="delete" buttonColor={theme.colors.errorContainer} textColor={theme.colors.onErrorContainer}>{deleteTagFromListAlert.yes[language]}</Button>
-                        <Button onPress={()=>toggleTagDeleteDialogVisible(false)} icon="cancel" buttonColor={theme.colors.secondary} textColor={theme.colors.onSecondary}>{deleteTagFromListAlert.no[language]}</Button>
-                    </Dialog.Actions>
-                </Dialog>
-                </View>}
-            buttonACK={<IconButton icon="check" onPress={() => setViewTagModal(false)} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>}
-            buttonCNL={null}
-            buttonDEL={null}
-        
-        />
-                          
-              
+            </TouchableNativeFeedback>
 
+            <ModalContainer visible={viewTagModal} setVisible={setViewTagModal}
+                title={tagModal.title[language]}
+                subtitle={tagModal.subtitle[language]}
+                content={
+                    <View style={{height: "100%", display: "flex", flexDirection: "column", alignContent: "flex-start"}}>
+                        <View style={{display: "flex", flexDirection: "row", alignItems: "center", padding: defaultViewPadding}}>
+                            <TextInput
+                                label={tagModal.inputTags[language]}
+                                value={text}
+                                onChangeText={text => setText(text)}
+                                style={{flex: 8, marginRight: 10}}
+                                returnKeyType='done'
+                                returnKeyLabel='OK'
+                            />
+                            <IconButton mode="contained" icon={"tag-plus"} size={30} onPress={()=>saveNewTag(null)} style={{ margin: 0}} />
+                        </View>
+
+                        <View style={{flex: 1}}>
+                            <View style={{ flex: 1, padding: defaultViewPadding}}>
+                                <Text style={{ width: "100%" }}>{tagModal.existingTags[language]}</Text>
+                                <Surface style={{ flex: 1 }}>
+                                    <ScrollView contentContainerStyle={{ flexDirection: "row", flexWrap: "wrap" }}>
+                                        {Array.from(new Set(itemTags.map((tag, index) => 
+                                            <View key={`${tag.label}_${index}`} style={{padding: 5}}>
+                                                <Chip onPress={()=>addTagFromList(tag.label)} onClose={()=>handleDeleteTagFromList(tag.label)}>{tag.label}</Chip>
+                                            </View>)
+                                        ))}
+                                    </ScrollView>
+                                </Surface>
+                            </View>
+                            <View style={{ flex: 1, padding: defaultViewPadding }}>
+                                <Text style={{ width: "100%" }}>{tagModal.selectedTags[language]}</Text>
+                                <Surface style={{ flex: 1 }}>
+                                    <ScrollView contentContainerStyle={{ flexDirection: "row", flexWrap: "wrap" }}>
+                                        {itemData.tags?.map((tag, index) =>{
+                                            return <View key={`${tag}_${index}`} style={{padding: 5}}><Chip onClose={()=>deleteTag(tag)}>{tag}</Chip></View>
+                                        })}
+                                    </ScrollView>
+                                </Surface>
+                            </View>
+                        </View>
+
+                        <Dialog visible={tagDeleteDialogVisible} onDismiss={()=>toggleTagDeleteDialogVisible(false)}>
+                            <Dialog.Title>
+                                {deleteTagFromListAlert.title[language]}
+                            </Dialog.Title>
+                            <Dialog.Content>
+                                <Text>{deleteTagFromListAlert.subtitle[language]}</Text>
+                            </Dialog.Content>
+                            <Dialog.Actions>
+                                <Button onPress={()=>deleteTagFromList()} icon="delete" buttonColor={theme.colors.errorContainer} textColor={theme.colors.onErrorContainer}>{deleteTagFromListAlert.yes[language]}</Button>
+                                <Button onPress={()=>toggleTagDeleteDialogVisible(false)} icon="cancel" buttonColor={theme.colors.secondary} textColor={theme.colors.onSecondary}>{deleteTagFromListAlert.no[language]}</Button>
+                            </Dialog.Actions>
+                        </Dialog>
+                    </View>
+                }
+                buttonACK={<IconButton icon="check" onPress={() => setViewTagModal(false)} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>}
+                buttonCNL={<IconButton icon="cancel" onPress={() => setViewTagModal(false)} style={{width: 50, backgroundColor: theme.colors.secondaryContainer}} iconColor={theme.colors.onSecondaryContainer}/>}
+                buttonDEL={null}
+            />
         </View>
     )
 }
