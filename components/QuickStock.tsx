a098b83c67626cef54db45879dcb0ed360af87a9
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-06-10 21:37:24 +0000

    Reqritten logic to acommodate for powder weight stock

--- components/QuickStock.tsx
+++ components/QuickStock.tsx
@@ -3,13 +3,14 @@ import { View } from "react-native";
 import { Button, Dialog, HelperText, IconButton, Text, TextInput } from "react-native-paper"
 import { usePreferenceStore } from "../stores/usePreferenceStore";
 import { dateTimeOptions, defaultViewPadding } from "../configs/configs";
-import { AmmoType, ReloadingType_Bullet } from "../lib/interfaces";
+import { AmmoType, ReloadingType_Bullet, ReloadingType_Powder } from "../lib/interfaces";
 import { ammoQuickUpdate, gunQuickShot } from "../lib/textTemplates";
 import { db } from "../db/client"
 import * as schema from "../db/schema"
 import { eq } from 'drizzle-orm';
 import { useItemStore } from "stores/useItemStore";
 import { v4 as uuidv4 } from 'uuid';
+import { convertWeightUnitsToMilligram, convertWeightUnitsToPreferredUnit } from "functions/utils";
 
 export default function QuickStock({navigation}){
 
@@ -22,35 +23,68 @@ export default function QuickStock({navigation}){
     const [seeInfo, toggleSeeInfo] = useState<boolean>(false)
     const [negativeAmount, setNegativeAmount] = useState<boolean>(false)
 
-    const quickStockItem = currentItem as AmmoType | ReloadingType_Bullet
+    const { preferredUnits } = usePreferenceStore()
 
-    async function saveNewStock(item:AmmoType | ReloadingType_Bullet){
+    const quickStockItem = currentItem as AmmoType | ReloadingType_Bullet | ReloadingType_Powder
+
+
+    async function saveNewStock(item:AmmoType | ReloadingType_Bullet | ReloadingType_Powder){
+      try{
         if(stockChange !== ""){
-        const currentValue:number = parseInt(item.currentStock) ? parseInt(item.currentStock) : 0
-        const increase:number = Number(input)
-        const total:number = stockChange === "inc" ? Number(currentValue) + Number(increase) : Number(currentValue) - Number(increase)
-        await db.update(schema[currentCollection]).set({currentStock: `${total}`, lastTopUpAt_unix: Date.now()}).where(eq(schema[currentCollection].id, item.id))
-        await db.insert(schema.logger).values({
-            id: uuidv4(),
-            createdAt: Date.now(), 
-            reference: quickStockItem.id,
-            collection: currentCollection,
-            changedField: "currentStock",
-            value_old: `${currentValue}`,
-            value_new: `${total}`,
-            snapshot: JSON.stringify(quickStockItem)
-        })
-        navigation.goBack()
-        displayError(false)
+          let currentValue:number
+          let total:number
+          if("currentStock" in item){
+            console.log("currentStock")
+            currentValue = parseInt(item.currentStock) ? parseInt(item.currentStock) : 0
+            const increase:number = Number(input)
+            total = stockChange === "inc" ? Number(currentValue) + Number(increase) : Number(currentValue) - Number(increase)
+            await db.update(schema[currentCollection]).set({currentStock: `${total}`, lastTopUpAt_unix: Date.now()}).where(eq(schema[currentCollection].id, item.id))
+          }
+          if("powderWeight" in item){
+            console.log("powderWeight")
+            currentValue = parseInt(item.powderWeight) ? parseInt(item.powderWeight) : 0 // this is Milligram
+            const increase:number = Number(convertWeightUnitsToMilligram(preferredUnits, "powderWeight", input))
+            total = stockChange === "inc" ? Number(currentValue) + Number(increase) : Number(currentValue) - Number(increase)
+            await db.update(schema[currentCollection]).set({powderWeight: `${total}`, lastTopUpAt_unix: Date.now()}).where(eq(schema[currentCollection].id, item.id))
+          }
+          await db.insert(schema.logger).values({
+              id: uuidv4(),
+              createdAt: Date.now(), 
+              reference: quickStockItem.id,
+              collection: currentCollection,
+              changedField: "currentStock" in item ? "currentStock" : "powderWeight",
+              value_old: `${currentValue}`,
+              value_new: `${total}`,
+              snapshot: JSON.stringify(quickStockItem)
+          })
+          navigation.goBack()
+          displayError(false)
       }
       else {
           displayError(true)
       }
+      }catch(e){
+              console.error(e)
+            }
     }
 
     function handleInput(input:string){
-      setNegativeAmount((quickStockItem.currentStock === undefined ? 0 : quickStockItem.currentStock === null ? 0 : Number(quickStockItem.currentStock)) < Number(input))
+       if("currentStock" in quickStockItem){
+        setNegativeAmount((quickStockItem.currentStock === undefined ? 0 : quickStockItem.currentStock === null ? 0 : Number(quickStockItem.currentStock)) < Number(input))
+        }
+      if("powderWeight" in quickStockItem){
+        setNegativeAmount((quickStockItem.powderWeight === undefined ? 0 : quickStockItem.powderWeight === null ? 0 : Number(convertWeightUnitsToPreferredUnit(preferredUnits, "powderWeight", quickStockItem.powderWeight))) < Number(input))
+      }
       setInput(input.replace(/[^0-9]/g, ''))
+    }
+
+    function setHelperText(){
+      if("currentStock" in quickStockItem){
+        return !quickStockItem.currentStock ? gunQuickShot.errorNoAmountDefined[language] : gunQuickShot.errorAmountTooLow[language].replace("{{AMOUNT}}", quickStockItem.currentStock)
+        }
+      if("powderWeight" in quickStockItem){
+        return !quickStockItem.powderWeight ? gunQuickShot.errorNoAmountDefined[language] : gunQuickShot.errorAmountTooLow[language].replace("{{AMOUNT}}", convertWeightUnitsToPreferredUnit(preferredUnits, "powderWeight", quickStockItem.powderWeight))
+      }
     }
     
     return(
@@ -67,10 +101,19 @@ export default function QuickStock({navigation}){
                       <IconButton mode="contained" icon="plus" selected={stockChange === "inc" ? true : false} onPress={()=>setStockChange("inc")}/>
                       <IconButton mode="contained" icon="minus" selected={stockChange === "dec" ? true : false} onPress={()=>setStockChange("dec")} />
                     </View>
-                    <TextInput style={{width: "100%"}} placeholder={ammoQuickUpdate.placeholder[language]} keyboardType={"number-pad"} value={input} onChangeText={input => handleInput(input)} inputMode='decimal' returnKeyType='done'
-                        returnKeyLabel='OK'/>
+                    <TextInput 
+                      style={{width: "100%"}} 
+                      placeholder={`${"powderWeight" in quickStockItem ? "" : ammoQuickUpdate.placeholder[language]}`} 
+                      keyboardType={"number-pad"} 
+                      value={input} 
+                      onChangeText={input => handleInput(input)} 
+                      inputMode='decimal' 
+                      returnKeyType='done'
+                      returnKeyLabel='OK'
+                      left={"powderWeight" in quickStockItem ? <TextInput.Affix text={`${preferredUnits.powderWeightUnit} `} />  : null}
+                    />
                         {negativeAmount  && stockChange === "dec" ? <HelperText type="error" visible={negativeAmount}>
-                                  {quickStockItem.currentStock === null ? gunQuickShot.errorNoAmountDefined[language] : quickStockItem.currentStock === undefined ? gunQuickShot.errorNoAmountDefined[language] : gunQuickShot.errorAmountTooLow[language].replace("{{AMOUNT}}", quickStockItem.currentStock)}
+                                  {setHelperText()}
                                 </HelperText> : null}
                     <View style={{width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: 10}}>
                     <IconButton disabled={negativeAmount && stockChange === "dec"} mode="contained" icon="check" onPress={() => saveNewStock(quickStockItem)} style={{width: 50, backgroundColor: theme.colors.primary}} iconColor={theme.colors.onPrimary}/>
