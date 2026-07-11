a2cef17e19c080db602a158bc4f8836528b8c99f
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-07-11 00:44:28 +0000

    Corrected translations

--- components/Dialogs/Onboarding/Onboarding_CountrySelect.tsx
+++ components/Dialogs/Onboarding/Onboarding_CountrySelect.tsx
@@ -15,11 +15,11 @@ export default function Onboarding_CountrySelect(){
     const [selectedCountry, setSelectedCountry] = useState<SupportedCountries>("ch")
 
     const countryHint: SimpleTranslation = {
-        de: "Je nach gewähltem Land werden einige länderspezifische Funktionen sichtbar. Diese können aber jederzeit in den Einstellungen individuell aus- oder eingeschaltet werden, auch unabhängig vom gewählten Land.",
-        en: "The language can be changed any time in the settings menu",
-        fr: "La langue peut être modifiée à tout moment dans les paramètres",
-        it: "La lingua può essere modificata in qualsiasi momento nelle impostazioni",
-        ch: "La lingua po vegnir midada da tut temp en las configuraziuns",
+        de: "Je nach gewähltem Land werden einige länderspezifische Funktionen sichtbar, hauptsächlich Klassifizierungen für Waffen und vordefinierte PDF-Listen.\n\nDas Land kann in den Einstellungen jederzeit ohne Datenverlust gewechselt werden; Die Datenbank unterscheidet nicht zwischen Ländern, es geht rein um die Darstellung.",
+        en: "Depending on the selected country, some country-specific features will become available, mainly firearm classifications and predefined PDF lists.\n\nThe country can be changed at any time in the settings without losing any data. The database does not distinguish between countries; this setting only affects the presentation.",
+        fr: "Selon le pays sélectionné, certaines fonctionnalités spécifiques au pays deviennent disponibles, principalement les classifications des armes et les listes PDF prédéfinies.\n\nLe pays peut être modifié à tout moment dans les paramètres sans perte de données. La base de données ne fait aucune distinction entre les pays ; ce réglage n'affecte que la présentation.",
+        it: "A seconda del Paese selezionato, saranno disponibili alcune funzionalità specifiche del Paese, principalmente le classificazioni delle armi e gli elenchi PDF predefiniti.\n\nIl Paese può essere modificato in qualsiasi momento nelle impostazioni senza alcuna perdita di dati. Il database non distingue tra i diversi Paesi; questa impostazione influisce esclusivamente sulla visualizzazione.",
+        ch: "Tut tenor il pajais tschernì vegnan activadas tschertas funcziuns specificas dal pajais, principalmain classificaziuns d'armas e glistas PDF predefinidas.\n\nIl pajais po vegnir midà da tut temp en las preferenzas senza perdita da datas. La banca da datas na fa nagina differenza tranter ils pajais; questa configuraziun influenzescha mo la preschentaziun.",
     }
 
     async function handleCountrySwitch(country:SupportedCountries){
