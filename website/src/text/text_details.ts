12e22d85fb8638530c034af86e404d7e849bd932
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-07-25 11:56:56 +0000

    New section "additional functionality"

--- website/src/text/text_details.ts
+++ website/src/text/text_details.ts
@@ -1,7 +1,7 @@
 import { getCaliberCount } from "../../../lib/caliberData";
 import { dataTemplate_Translations } from "../../../lib/DataTemplates/translations";
 import { tabBarLabels } from "../../../lib/Text/text_tabBarLabels"
-import type { Language, SimpleTranslation, SimpleTranslation_StringArray, SimpleTranslation_StringArrayName } from "../types/types_global";
+import type { functionalityIcons, Language, SimpleTranslation, SimpleTranslation_StringArray, SimpleTranslation_StringArrayName } from "../types/types_global";
 import { dataTemplateConverter } from "../utils/dataTemplateConverter";
 import { emptyGunObject } from "../../../lib/DataTemplates/gunDataTemplate"
 import { emptyAmmoObject } from "../../../lib/DataTemplates/ammoDataTemplate"
@@ -280,3 +280,327 @@ export const section_supportedCalibers: Section = {
         ]
     }
 }
+
+export const section_supportedFunctionality = {
+    title: {
+        de: ``,
+        en: `Additional Functionality`,
+        fr: ``,
+        it: ``
+    },
+    text: {
+        de: `
+        `,
+        en: `
+Arsenal offers a wide variety of additional functionality that goes beyond what a simple gun collection tracker offers and which sets it apart from competitor applications.
+        `,
+        fr: `
+        `,
+        it: `
+        `,
+    }
+}
+
+export const section_supportedFunctionality_details: {title: SimpleTranslation, text: SimpleTranslation, icon: functionalityIcons}[] = [
+    {
+        title: {
+            de: ``,
+            en: `QuickShot - Keep track of how may rounds went through your gun`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+QuickShot is a "rangelog light" and allows to log shots taken with a firearm directly from the collection overview.
+It is connected to the ammunition in the app, meaning QuickShot detects if you have caliber compatible ammunition in stock.
+This allows you to not only quickly log a shooting session, but also directly subtract the spent rounds from your ammunition stock.
+
+Of course, you can just log shots without touching your ammunition stock.
+Using QuickShot will update the used firearm with shots fired and date of last shooting and the ammuniton, if applicable.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "quickShot"
+    },
+    {
+        title: {
+            de: ``,
+            en: `QuickStock - Keep track of your consumables' stock increase and decrease`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+QuickStock is a smart utility to quickly top up your consumables (ammunition, bullets, casings, primers, powder) directly from the collection overview.
+Not only can you quickly log increase and decrease of stock, you can also add how much you paid for your top-up.
+In the detail view of your consumables, you will find a "costs" tab, which lists all top-ups, how much you paid and what the average price is.
+
+For powder, you can specify the quantity unit. This is so you can log a decrease in grains despite having the powder amount unit set to pounds, for example.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "quickStock"
+    },
+        {
+        title: {
+            de: ``,
+            en: `QuickMount - Keep track of where your accessories or weapon parts are mounted to`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+QuickMount allows you to quickly "mount" an accessory or a weapon part to a gun, another accessory or another weapon part.
+This way, you always know where an accessory is mounted on, or which weapon has which accessories attached.
+A convenient, categorized list (with images) makes it easy to quickly select your target gun, accessory or weapon part.
+
+In the tile view image, on the bottom, you will find icons corresponding to the type of accessory or weapon part which is mounted.
+In the detail view under the tab "accessories" you will find the information of the accessory or weapon part and you can unmount or remount the accessory or weapon part.
+In the tile view image, on the top, it is shown on which accessory, weapon part or weapon something is currently mounted to.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "quickMount"
+    },
+    {
+        title: {
+            de: ``,
+            en: `QR Code Integration & Creation`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+QR Codes are a quick and easy way to physically track your inventory. 
+
+If you already employ QR Codes in your collection, be it codes on a tag on your guns, or code stickers on ammo boxes, simply scan the existing code when creating a new collection entry and that code is now linked to it. 
+Use the scan button in the collection overview to scan your code and the linked collection item is displayed immediately - no matter where you are in the app.
+
+But what if you dont already have QR codes, but _want_ to? 
+No problem, Arsenal features a complete customizable QR Code Label generator! 
+Select which collection you want to generate codes for, which items specifically (or just everything), select which attributes should appear on the label (or none if you just want the code) and the format and there you go!
+Arsenal generates a PDF with the QR Code Tags ready to print! 
+
+There are several predefined formats (the most common Avery/Zweckform) in either DIN A4 or US Letter format, but you can of course create and save your own custom format, just like in Microsoft Office Word! 
+Dont want to deal iwth QR Codes but labels would be neat? No problem, just leave the QR Code off!
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "qrCode"
+    },
+        {
+        title: {
+            de: ``,
+            en: `Database import/Export`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+A core functionality of every collection tracker is importing/exporting existing databases from and to the app. 
+Arsenal uses SQLite under the hood, a fast and stable database standard used in millions of applications. 
+
+Export:
+You can export the Arsenal Database in the following formats:
+- __Full Database Export__
+  This generates a zipped folder with your raw database in SQlite and a folder with all your images. This is the standard format you should use to back up your database and when switching devices.
+- __CSV Export__
+  This generates a CSV file of the collection you specified. This is useful if you wnat physical printouts or if you want to amend your database on a computer.
+  It does not include images, though, only the references to their location on your device. 
+  However if you reimport the CSV and you did not delete any data on the Arsenal app, the images will be correctly linked again.
+
+Import:
+You can import databases into Arsenal in the following formats:
+- __Full Database Import__
+  This imports the zipped folder with the database and images. It requires a certain naming convention and file format; This is set automatically on the full database export.
+- __Arsenal CSV Import__
+  This imports the file generated by the CSV Export. It requires a certain naming convention and file format and the CSV Export needs to have been generated by the same app version.
+  This is set automatically on the Arsenal CSV Export.
+- __Custom CSV Import__
+  This imports any CSV file you desire. You have to manually map the CSV column names to the Arsenal database column names; This can be done through a convenient helper dialog in the app.
+  Use this if you want to move your database from a third party gun collection app, from your computers Excel file or from a lower version Arsenal CSV.
+  
+  Please make sure that the \`caliber\` field in your CSV matches the expected caliber notation of Arsenal, otherwise the QuickStock functionality will be impacted and other undesireable sideeffects may appear.  
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "importExport"
+    },
+        {
+        title: {
+            de: ``,
+            en: `PDF Lists`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+Another core functionality of every decent collection tracker are PDF list exports. Arsenal offers two universal PDF lists:
+- __Complete Gun List as a table__
+  This generates a table with all your guns with the following attributes:
+  - Manufacturer
+  - Model Name
+  - Caliber
+  - Serial
+  - Permit reference
+  - Acquisition Date
+  
+  It is a quick and easy way to provide a complete list of your guns, be it for insurance purposes, on agency request or for whatever you might need it
+
+- __Custom List__
+This lets you print a PDF list of any collection you want with any attributes you want. Need a list of all your guns for your insurance and want to include
+the value, but not the serial? No problem! Want a physical list of your ammo collection with just the Designation and the caliber? Here you go!
+
+Depending on your selected country of residence, additional predefined lists are offered. Currently, these are:
+- Switzerland
+  - __Gun List according to WA Article 5 as a table__
+    This generates a table with all your guns that fall under Article 5 with the following attributes:
+    - Manufacturer
+    - Model Name
+    - Caliber
+    - Serial
+    - Permit reference
+    - Acquisition Date
+    - Reason why it is under Aticle 5
+    This is very useful if you apply for a collectors permit or if your local weapons bureau wants an excerpt of your prohibited items.
+
+  - __Complete Gun List as a table with WA Article 5 properties__
+    This generates a table with all your guns including wether or not thes fall under Article 5 and why. Its a hybrid between the Complete Gun List and the Article 5 List.
+    It exists because it was requested by some users since some weapons bureaus want a complete list of your collection, not only of the guns that fall under Article 5. 
+    This list adds additional detail.
+
+Also, each collection item can be printed as a "gallery print", which displays every image, any tags, and all attributes. This is useful if you want to have a physical representation for display purposes or if you want to sell something and need a flyer for your clubs blackboard.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "pdf"
+    },{
+        title: {
+            de: ``,
+            en: `Customizable Units`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+Depending on where you live on this planet, you are used to certain units of measurements. However, sometimes, you still want to or have to use other units. Arsenal leaves you the freedom to decide which length or weight unit for what purpose you want to display. 
+
+Everything in metric except powder? No problem. Bullet weight in ounces? Weird choice, but you do you. 
+To account for future additional unit attributes, a general weight and length unit is also provided. 
+
+You can change these values anytime. The database strictly saves every value in Milligram and Millimeter respectively and converts it to your desired display unit.
+
+For powder, it is recommended that you set the weight unit to the commonly used commercial units - Kilograms or Grams for metric countries, Pounds or Ounces for imperial countries.
+For any stock increase or decrease, you can select your preferred unit again - this is to accomodate stock increase in for example Kilograms, but decrease in Grains when you used some powder for reloading.
+
+As for currencies, you can set any world currency you like. 
+However it is merely a cosmetic choice, as the database only saves the numeric value and doesnt associate it with any currency conversions or inflation percentages. 
+This is due to the strict offline handling of the app.
+
+Most people use only one currency anyways. If you bought something in another currency, it is recommended you enter the buy price converted to your currency.
+
+As for inflation, it is planned to integrate yearly inflation percentages for every country directly into the app with the caveat that data only starts at 1980.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "units"
+    },
+    {
+        title: {
+            de: ``,
+            en: `Item History`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+Every change, every edit you make for a collection item is logged, so you always have a complete and transparent history for your item.
+Each log entry displays exactly when a change was made, what changed, what the old value was and what the new value is.
+
+Not only is this useful to trace the entire history of an item, it also acts as a save point when you accidentally enter a wrong value and dont remember the original value.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "history"
+    },
+    {
+        title: {
+            de: ``,
+            en: `Cost History`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+Consumables (Ammunition, Bullets, Cases, Primers, Powder) also feature a Cost History so you see exactly when you spent what on which item.
+
+It displays the purchase date, the total amount, the total price paid and an average price. That way you can monitor your spending and any price trends.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "costHistory"
+    },
+    {
+        title: {
+            de: ``,
+            en: `Mark as Sold`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+Its unheard of, but it can happen that something of your collection got sold to someone else. 
+Arsenal (reluctantly) offers a functionality to mark an item as sold, but keeps it in your database. You can choose if you want to have it still displayed in our collection (greyed out) or have it hidden completely. 
+
+When "selling" something, you can enter the details of the transaction, such as buyer name, sell date, sell price, any permit references and personal notes.
+These details cannot be amended later, so make sure they are correct. They will be displayed on top of all other attributes when you view a sold item in your collection.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "sold"
+    },
+    {
+        title: {
+            de: ``,
+            en: `Tags`,
+            fr: ``,
+            it: ``,
+        },
+        text: {
+            de: ``,
+            en: `
+Tags are a convenient way to group items in a collection. 
+
+If for example you want to filter your book collection for the topic "Swiss Military Weapons", simply tag the corresponding collection items and apply the collection filter. 
+Its also useful to group your weapons by type. Everyone has their own preferences: 
+One just wants a general distiction between rifles, Pistols and Revolvers. Another wants to distinguish deeper - Toploading revolvers, Cap & Ball Revolvers, Magnum Revolvers, etc...
+With tags, you can decide on your own framework of weapon classifications.
+
+You can use any tags you like. You also do not need to write them a hundred times over; Arsenal offers a convenient dialog to manage the tags and it keeps track of existing ones.
+`,
+            fr: ``,
+            it: ``,
+        },
+    icon: "tags"
+    },
+]
