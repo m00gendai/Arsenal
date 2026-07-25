ae40d01a5b543527f781a95e11d834496e0fb075
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-07-25 12:00:37 +0000

    New section additional functionality, added dividers

--- website/src/components/Details/details.tsx
+++ website/src/components/Details/details.tsx
@@ -1,7 +1,9 @@
 import { title } from "../../text/text_details"
 import type { Language } from "../../types/types_global"
+import Divider from "../Divider"
 import Details_supportedCalibers from "./details_supportedCalibers"
 import Details_supportedCollections from "./details_supportedCollections"
+import Details_supportedFunctionality from "./details_supportedFunctionality"
 
 interface Props{
     language: Language
@@ -13,6 +15,12 @@ export default function DetailsWrapper({language}:Props){
             <h1>{title[language]}</h1>
 
             <Details_supportedCollections language={language} />
+
+            <Divider height={50} />
+
+            <Details_supportedFunctionality language={language} />
+
+            <Divider height={50} />
 
             <Details_supportedCalibers language={language} />
 
