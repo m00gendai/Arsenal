2dbce202fcf3dbfde010acc45b94e5a252dae662
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-09-06 07:25:25 +0000

    Added ch_permitTypes (unused for now)

--- lib/dropDownPickerOptions.ts
+++ lib/dropDownPickerOptions.ts
@@ -8,6 +8,7 @@ interface DropDownPickerOptionsEntry {
 
 export interface DropDownPickerOptions {
     de_wbkColor: DropDownPickerOptionsEntry
+    ch_permitTypes: DropDownPickerOptionsEntry
 }
 
 export const dropDownPickerOptions: DropDownPickerOptions = {
@@ -36,6 +37,43 @@ export const dropDownPickerOptions: DropDownPickerOptions = {
             { label: '🟩 Verd', value: 'green' },
             { label: '🟨 Mellen', value: 'yellow' },
             { label: '🟥 Cotschen', value: 'red' },
+        ],
+    },
+    ch_permitTypes: {
+        de: [
+            { label: 'Vertrag', value: 'contract' },
+            { label: 'WES - Waffenerwerbsschein', value: 'wes' },
+            { label: 'ABK Sport - Ausnahmebewilligung klein', value: 'abk1' },
+            { label: 'ABK Sammler - Ausnahmebewilligung klein', value: 'abk2' },
+            { label: 'SON - Sonderbewilligung', value: 'son' },
+        ],
+        en: [
+            { label: 'Contract', value: 'contract' },
+            { label: 'WAP - Weapons Acquisition Permit', value: 'wes' },
+            { label: 'EP Sport - Small Exceptional Permit for Sport Shooting', value: 'abk1' },
+            { label: 'EP Collector - Small Exceptional Permit for Collectors', value: 'abk2' },
+            { label: 'EP - Exceptional Permit', value: 'son' },
+        ],
+        fr: [
+            { label: 'Contrat', value: 'contract' },
+            { label: 'PAA - Permis d’acquisition d’armes', value: 'wes' },
+            { label: 'PAE Sport - Autorisation exceptionnelle pour tireur sportif', value: 'abk1' },
+            { label: 'PAE Collectionneur - Autorisation exceptionnelle pour collectionneur', value: 'abk2' },
+            { label: 'PAE - Autorisation exceptionnelle', value: 'son' },
+        ],
+        it: [
+            { label: 'Contratto', value: 'contract' },
+            { label: 'PAA - Permesso d’acquisto di armi', value: 'wes' },
+            { label: 'AE Sportivo - Permesso di esenzione per il tiro sportivo', value: 'abk1' },
+            { label: 'AE Collezionista - Autorizzazione cantonale eccezionale per motivi di collezionismo', value: 'abk2' },
+            { label: 'AE - Autorizzazione d’eccezione', value: 'son' },
+        ],
+        ch: [
+            { label: 'Contract', value: 'contract' },
+            { label: 'PAA - Permissiun d’acquistar armas', value: 'wes' },
+            { label: 'PCE Sport - Permissiun chantunala excepziunala Tir da sport', value: 'abk1' },
+            { label: 'PCE Collecziun - Permissiun chantunala excepziunala Collecziunaders', value: 'abk2' },
+            { label: 'PCE - Permissiun chantunala excepziunala', value: 'son' },
         ],
     }
 }
