9387e39281e51b5f249e69424ecc93a475996f6c
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2026-09-17 02:31:35 +0000

    Wrapped sliders in View, added setting to select tile columns

--- components/MainMenu/Settings/Settings_Display.tsx
+++ components/MainMenu/Settings/Settings_Display.tsx
@@ -1,15 +1,28 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { defaultViewPadding } from "configs/configs";
+import { defaultViewPadding, numberBadgeCollections } from "configs/configs";
 import { PREFERENCES } from "configs/configs_DB";
 import { displaySettingsLabels, preferenceTitles } from "lib/Text/text_settings";
 import { View } from "react-native";
 import { Divider, List, Switch, Text } from "react-native-paper";
 import { usePreferenceStore } from "stores/usePreferenceStore";
-import { useViewStore } from "stores/useViewStore"
+import { SegmentedButtons } from 'react-native-paper';
+import { useState } from "react";
+import { Dropdown } from "react-native-paper-dropdown";
 
 export default function Settings_Display(){
 
     const { theme, language, generalSettings, setGeneralSettings } = usePreferenceStore()
+    const [value, setValue] = useState<number>(generalSettings.gridColumns)
+
+    async function handleRange(range: string, value: number){
+        // DISPLAY SETTINGS ARE STORED AS GENERAL SETTINGS, the distinction is only made for UI reasons
+        const newSettings = {...generalSettings, [range]: value}
+        setGeneralSettings(newSettings)
+        const preferences:string = await AsyncStorage.getItem(PREFERENCES)
+        const newPreferences:{[key:string] : string} = preferences == null ? {"generalSettings": newSettings} : {...JSON.parse(preferences), "generalSettings": newSettings} 
+        await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
+        setValue(value)
+    }
 
     async function handleSwitches(setting: string){
         // DISPLAY SETTINGS ARE STORED AS GENERAL SETTINGS, the distinction is only made for UI reasons
@@ -20,6 +33,8 @@ export default function Settings_Display(){
         await AsyncStorage.setItem(PREFERENCES, JSON.stringify(newPreferences))
     }
 
+
+
     return( 
         <View style={{backgroundColor: theme.colors.tertiaryContainer}}>
             <List.Accordion left={props => <List.Icon {...props} icon="monitor-dashboard" />} title={preferenceTitles.displaySettings[language]} titleStyle={{fontWeight: "700", color: theme.colors.onSecondaryContainer}} style={{paddingLeft: defaultViewPadding*2, backgroundColor: theme.colors.secondaryContainer, borderColor: theme.colors.primary, borderLeftWidth: 5}}>
@@ -27,35 +42,67 @@ export default function Settings_Display(){
                     <View style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", flexWrap: "wrap", gap: 5}}>
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{displaySettingsLabels.displayImagesInListView[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.displayImagesInListView} onValueChange={()=>handleSwitches("displayImagesInListView")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.displayImagesInListView} onValueChange={()=>handleSwitches("displayImagesInListView")} /></View>
                         </View>
                         
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{displaySettingsLabels.titleBelowImage[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.titleBelowImage} onValueChange={()=>handleSwitches("titleBelowImage")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.titleBelowImage} onValueChange={()=>handleSwitches("titleBelowImage")} /></View>
                         </View>
                         
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{displaySettingsLabels.emptyFields[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.emptyFields} onValueChange={()=>handleSwitches("emptyFields")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.emptyFields} onValueChange={()=>handleSwitches("emptyFields")} /></View>
                         </View>
                         
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
     
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{displaySettingsLabels.caliberDisplayName[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.caliberDisplayName} onValueChange={()=>handleSwitches("caliberDisplayName")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.caliberDisplayName} onValueChange={()=>handleSwitches("caliberDisplayName")} /></View>
                         </View>
 
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
  
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{displaySettingsLabels.displaySoldItems[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.displaySoldItems} onValueChange={()=>handleSwitches("displaySoldItems")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.displaySoldItems} onValueChange={()=>handleSwitches("displaySoldItems")} /></View>
+                        </View>
+
+                        <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
+ 
+                        <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
+                            <Text style={{flex: 7}}>{displaySettingsLabels.gridColumns[language]}</Text>
+<View style={{flex: 4, flexDirection: "row", justifyContent: "flex-end"}}><View style={{width: "50%"}}><Dropdown
+                            label={displaySettingsLabels.gridColumnsLabel[language]}
+                            placeholder={``}
+                            options={[
+                                {
+                                    label: "1",
+                                    value: "1"
+                                },
+                                {
+                                    label: "2",
+                                    value: "2"
+                                },
+                                {
+                                    label: "3",
+                                    value: "3"
+                                },
+                                {
+                                    label: "4",
+                                    value: "4"
+                                },
+                            ]}
+                            value={`${value}`}
+                            onSelect={(newValue) => handleRange("gridColumns", Number(newValue))}
+
+                        /></View>
+                        </View>
                         </View>
                         
                     </View>
--- components/MainMenu/Settings/Settings_General.tsx
+++ components/MainMenu/Settings/Settings_General.tsx
@@ -53,28 +53,28 @@ export default function Settings_General(){
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{generalSettingsLabels.resizeImages[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.resizeImages} onValueChange={()=>generalSettings.resizeImages ? handleSwitchesAlert("resizeImages") : handleSwitches("resizeImages")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.resizeImages} onValueChange={()=>generalSettings.resizeImages ? handleSwitchesAlert("resizeImages") : handleSwitches("resizeImages")} /></View>
                         </View>
                         
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{generalSettingsLabels.loginGuard[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.loginGuard} onValueChange={()=>handleSwitchesAlert("loginGuard")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.loginGuard} onValueChange={()=>handleSwitchesAlert("loginGuard")} /></View>
                         </View>
 
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{generalSettingsLabels.hintsDisplay[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.hintsDisplay} onValueChange={()=>handleSwitches("hintsDisplay")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.hintsDisplay} onValueChange={()=>handleSwitches("hintsDisplay")} /></View>
                         </View>
 
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
                         
                         <View style={{display: "flex", flexWrap: "nowrap", justifyContent: "space-between", alignItems: "center", flexDirection: "row", width: "100%"}}>
                             <Text style={{flex: 7}}>{generalSettingsLabels.rememberLastScreen[language]}</Text>
-                            <Switch style={{flex: 3}} value={generalSettings.rememberLastScreen} onValueChange={()=>handleSwitches("rememberLastScreen")} />
+                            <View style={{flex: 3, flexDirection: "row", justifyContent: "flex-end"}}><Switch value={generalSettings.rememberLastScreen} onValueChange={()=>handleSwitches("rememberLastScreen")} /></View>
                         </View>
 {/*
                         <Divider style={{width: "100%", borderWidth: 0.5, borderColor: theme.colors.onSecondary}} />
